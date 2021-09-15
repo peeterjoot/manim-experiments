32e594a3be404f89c53312e8ab800e1434a5e48a
Peeter Joot <peeter.joot@lzlabs.com>
2021-09-15 19:21:33 -0400

    parallelogram.py    don't use raw python tuples.  Use numpy arrays instead (which have    dot, +, -, norm, ... available.)

--- examples/parallelogram.py
+++ examples/parallelogram.py
@@ -1,7 +1,7 @@
 from manim import *
-from operator import add, sub
 import numpy as np
-import math as libm
+#from operator import add, sub
+#import math as libm
 
 #   p1 + p2
 #     /\
@@ -13,11 +13,11 @@ import math as libm
 
 class DrawParallelogram(Scene):
     def construct(self):
-        o = (0, -2, 0)
-        p1 = (2, 1, 0)
-        p2 = (-0.5, 3, 0)
-        op1 = tuple(map(add, o, p1))
-        op2 = tuple(map(add, o, p2))
+        o = np.array([0, -2, 0])
+        p1 = np.array([2, 1, 0])
+        p2 = np.array([-0.5, 3, 0])
+        op1 = o + p1
+        op2 = o + p2
 
         v1 = Arrow(start=o, end=op1, buff=0)
         v2 = Arrow(start=o, end=op2, buff=0)
@@ -27,18 +27,16 @@ class DrawParallelogram(Scene):
         v1l.next_to(v1, RIGHT)
         v2l.next_to(v2, UP)
 
-        s = libm.sqrt(np.dot(p1, p1))
-        p1cap = [element / s for element in p1]
-        p1dotp1cap = np.dot(p2, p1cap)
-        proj = [element * p1dotp1cap for element in p1cap]
-        oproj = tuple(map(add, o, proj))
+        p1cap = p1/ np.linalg.norm(p1)
+        proj = np.dot(p2, p1cap) * p1cap
+        oproj = o + proj
         vproj = Line(start=o, end=oproj, color=RED)
         vprojl = Tex(r'$\left(\vec{b} \cdot \hat{a}\right) \hat{a}$').scale(1)
         vprojl.next_to(vproj, DOWN)
         v1g = VGroup(v1, v1l)
         vprojg = VGroup(vproj, vprojl)
 
-        rej = tuple(map(sub, p2, proj))
+        rej = p2 - proj
         vrej = Line(start=oproj, end=op2, color=RED)
         vrejl = Tex(r'$\vec{b} - \left(\vec{b} \cdot \hat{a}\right) \hat{a}$').scale(1)
         vrejl.next_to(vrej, RIGHT)
@@ -46,12 +44,12 @@ class DrawParallelogram(Scene):
         v2g = VGroup(v2, v2l)
         vrejg = VGroup(vrej, vrejl)
 
-        op3 = tuple(map(add, op1, p2))
+        op3 = op1 + p2
         parallelogram = [o, op1, op3, op2]
         poly = Polygon(*parallelogram)
 
-        cut = tuple(map(add, op1, rej))
-        recttop = tuple(map(sub, cut, p1))
+        cut = op1 + rej
+        recttop = cut - p1
         dashrej = DashedLine(start=op1, end=cut)
         dashtop = DashedLine(start=cut, end=recttop)
         dashside = DashedLine(start=recttop, end=o)
