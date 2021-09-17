00489945517c028f25e52c9a31ae579d071c0051
Peeter Joot <peeter.joot@lzlabs.com>
2021-09-16 21:00:09 -0400

    modified:   Makefile new file:   ParallelogramComputationGA.py

---
+++ examples/ParallelogramComputationGA.py
@@ -0,0 +1,49 @@
+from manim import *
+import numpy as np
+
+def concat(*args, sep=''):
+    return sep.join(args)
+def mknorm(*args, sep=''):
+    return concat( r'\left\lVert{', sep.join(args), r'}\right\rVert' )
+def mknorm2(*args, sep=''):
+    return concat( r'{', mknorm( sep.join(args) ), r'}^2' )
+def lr(*args, sep=''):
+    return concat( r'\left( ', sep.join(args), r' \right)' )
+def lrsq(*args, sep=''):
+    return concat( r'{', lr( sep.join(args) ), r'}^2' )
+def sq(*args, sep=''):
+    return concat( r'{', sep.join(args), r'}^2' )
+def dot(a, b):
+    return a + r' \cdot ' + b
+
+class ParallelogramComputationGA(Scene):
+    def construct(self):
+        vecu = r'\vec{u}'
+        uhat = r'\hat{u}'
+        vecv = r'\vec{v}'
+        squ = sq( vecu )
+        sqv = sq( vecv )
+        lbr  = r'\left('
+        rbr  = r'\right)'
+        vdotusq = lrsq( dot( vecv, uhat ) )
+        nextline = r'\\'
+        vwedgeuhat = concat( vecv, r'\wedge', uhat )
+        vwedgeu = concat( vecv, r'\wedge', vecu )
+        uwedgev = concat( vecu, r'\wedge', vecv )
+        rej = concat( lr( vwedgeuhat ), uhat )
+
+        eq = MathTex( r'\text{Area} &= \text{base} \times \text{height} \\',
+                      concat( r'{\text{Area}}^2 &= ', squ, mknorm2( rej ), nextline ),
+                      concat( '&= ', squ, mknorm2( vwedgeuhat ), nextline ),
+                      concat( '&= ', mknorm2( vwedgeu ), nextline ),
+                      concat( '&= ', dot( lr(vwedgeu), lr(uwedgev) ), nextline ),
+                      concat( '&= ', squ, sqv, '-', lrsq( dot( vecv, vecu ) ), nextline )
+                    )
+
+        self.play( Write( eq[0] ) )
+        self.play( Write( eq[1] ) )
+        self.play( Write( eq[2] ) )
+        self.play( Write( eq[3] ) )
+        self.play( Write( eq[4] ) )
+        self.play( Write( eq[5] ) )
+        self.wait( )
