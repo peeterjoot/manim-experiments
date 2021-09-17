56e6ea22fa923105f5bdb3803db6dc1ce7a097e5
Peeter Joot <peeter.joot@lzlabs.com>
2021-09-16 20:39:26 -0400

    help from sudgy: how to handle alignment more naturally.

--- examples/ParallelogramComputationClassic.py
+++ examples/ParallelogramComputationClassic.py
@@ -11,8 +11,8 @@ def lrsq(v):
     return r'{\left( ' + v + r' \right)}^2'
 def dot(a,b):
     return a + r' \cdot ' + b
-def math(a):
-    return '$' + a + '$'
+def concat(*args, sep=''):
+    return sep.join(args)
 
 class ParallelogramComputationClassic(Scene):
     def construct(self):
@@ -21,37 +21,22 @@ class ParallelogramComputationClassic(Scene):
         vecv = r'\vec{v}'
         nsqu = mknorm2( vecu )
         nsqv = mknorm2( vecv )
-        eq   = math( r'\,=\,' )
-        nbsp = r'$\,$' # alignment with empty string doesn't seem to work
-
-        eq1_text = [ math( r'\text{Area}' ), eq, math( r'\text{base} \times \text{height}' ) ]
-        eq2_text = [ math( r'{\text{Area}}^2' ), eq, '${}{}$'.format( nsqu, mknorm2(r'{} - \left({} \cdot {} \right){}'.format(vecv, vecv, uhat, uhat)) ) ]
+        lbr  = r'\left('
+        rbr  = r'\right)'
         vdotusq = lrsq( dot( vecv, uhat ) )
-        eq3_text = [ nbsp, eq, math( nsqu + r'\left(' + nsqv + ' + ' + vdotusq + '- 2 ' + vdotusq + r'\right)' ) ]
+        nextline = r'\\'
+        rej = concat( vecv, '-', lbr, dot( vecv, uhat ), rbr, uhat )
 
-        eq1_mob = Tex( *eq1_text )
-        eq2_mob = Tex( *eq2_text )
-        eq3_mob = Tex( *eq3_text )
+        eq = MathTex( r'\text{Area} &= \text{base} \times \text{height} \\',
+                      concat( r'{\text{Area}}^2 &= ', nsqu, mknorm2( rej ), nextline ),
+                      concat( '&= ', nsqu, lbr, nsqv, ' + ', vdotusq, '- 2 ', vdotusq, rbr, nextline ),
+                      concat( '&= ', nsqu, lbr, nsqv, ' - ', vdotusq, rbr, nextline ),
+                      concat( '&= ', nsqu, nsqv, ' - ', lrsq( dot( vecv, vecu ) ), nextline )
+                    )
 
-        for i,item in enumerate( eq2_mob ):
-            dir = RIGHT
-            if i == 2:
-                dir = LEFT
-            item.align_to( eq1_mob[i], dir )
-
-        for i,item in enumerate( eq3_mob ):
-            dir = RIGHT
-            if i == 2:
-                dir = LEFT
-            item.align_to( eq2_mob[i], dir )
-
-        eq1 = VGroup( *eq1_mob )
-        eq1.shift( 3*LEFT + 2*UP )
-        eq2 = VGroup( *eq2_mob )
-        eq2.shift( UP + 3*LEFT )
-        eq3 = VGroup( *eq3_mob )
-        eq3.shift( 3*LEFT )
-        self.play( Write( eq1 ) )
-        self.play( Write( eq2 ) )
-        self.play( Write( eq3 ) )
-        self.wait( 6 )
+        self.play( Write( eq[0] ) )
+        self.play( Write( eq[1] ) )
+        self.play( Write( eq[2] ) )
+        self.play( Write( eq[3] ) )
+        self.play( Write( eq[4] ) )
+        self.wait( )
--- python/tex.py
+++ python/tex.py
@@ -3,5 +3,12 @@
 def mknorm(v):
     return r'\left\lVert{' + v + r'}\right\rVert'
 
+def concat(*args, sep=' '):
+    return sep.join(args)
+
 b = mknorm('a');
 print(b)
+
+print(concat('hello', 'world', sep=''))
+print(concat('hello', 'world', sep='_'))
+print(concat('hello', 'world'))
