3b1dfd1ae2f24d196f55e9f616a910f3ab922ca3
Peeter Joot <peeter.joot@lzlabs.com>
2021-09-25 22:24:20 -0400

    modified:   3D.py

--- experiments/3D.py
+++ experiments/3D.py
@@ -27,9 +27,16 @@ class ThreeDSurfacePlot(ThreeDScene):
             u_range=[-2, +2]
         )
 
+        # looks like manim gets phi vs. theta mixed up.  phi seems to be the angle from the z-axis
         gauss_plane.scale_about_point(2, ORIGIN)
         gauss_plane.set_style(fill_opacity=1,stroke_color=GREEN)
         #gauss_plane.set_fill_by_checkerboard(ORANGE, BLUE, opacity=0.5)
         axes = ThreeDAxes()
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
         self.add(axes,gauss_plane)
+        self.begin_ambient_camera_rotation(rate=0.1)
         self.wait()
+        self.stop_ambient_camera_rotation()
+        self.move_camera(phi=100 * DEGREES, theta=60 * DEGREES)
+        self.wait()
+
