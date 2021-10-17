23c713da1d61a638250b80c7b4fd44093ae4bfc5
Peeter Joot <peeter.joot@lzlabs.com>
2021-10-17 13:55:55 -0400

    flush out animation for ProjRej2 a bit and add delays.

--- parallelogram/parallelogram.py
+++ parallelogram/parallelogram.py
@@ -598,18 +598,56 @@ class ProjRej2( Scene ):
         rej        = l.mult( lr_vwedgeu, invu )
         proj2      = l.mult( l.lr( l.dot( vecv, hatu ) ), hatu )
         rej2       = l.mult( l.lr( l.wedge( vecv, hatu ) ), hatu )
+        projr      = l.mult( invu, lr_udotv )
+        rejr       = l.mult( invu, lr_uwedgev )
 
-        eq = MathTex( concat( l.Proj( vecu, vecv ), ' &\equiv ', proj, l.newline ),
-                      concat( l.Rej( vecu, vecv ),  ' &\equiv ', rej, l.newline ) )
-        eq2 = MathTex( concat( l.Proj( vecu, vecv ), ' &\equiv ', proj, ' = ', proj2, l.newline ),
-                       concat( l.Rej( vecu, vecv ),  ' &\equiv ', rej, ' = ', rej2, l.newline ) )
+        eq = MathTex( l.Proj( vecu, vecv ), ' &\equiv ', proj, l.newline,
+                      l.Rej( vecu, vecv ),  ' &\equiv ', rej, l.newline )
+        eqreversed = MathTex( concat( '&=', projr, l.newline ),
+                              concat( '&=', rejr, l.newline ) )
+        equnit = MathTex( concat( '&=', proj2, l.newline ),
+                          concat( '&=', rej2, l.newline ) )
+        eq[2].set_color( BLUE )
+        eq[6].set_color( BLUE )
 
-        for item in eq:
-            self.play( Write( item ) )
+        for i in range(0, 3):
+            self.play( Write( eq[i] ) )
+        self.wait(3)
+        # +7s
 
-        self.play( ReplacementTransform( eq, eq2 ) )
+        for i in range(4, 8):
+            self.play( Write( eq[i] ) )
+        self.wait(2)
+        # +7s
 
-        self.wait( )
+        self.wait(5)
+        eqreversed.shift( 2.5 * RIGHT )
+        eqreversed.set_color( TEAL )
+        self.play( eq.animate.shift( 1.3 * LEFT ) )
+        for i in range(2):
+            self.play( Write( eqreversed[i] ) )
+        self.wait( 5 )
+
+        howorig = MathTex( concat( vecv, ' = ', vecv, vecu, invu ) )
+        howorig.set_color( BLUE )
+        howorig.move_to( 2 * UP + 0.2 * LEFT )
+
+        howreversed = MathTex( concat( vecv, ' = ', invu, vecu, vecv ) )
+        howreversed.set_color( TEAL )
+        howreversed.move_to( 2 * UP + 2.8 * RIGHT )
+        self.wait( 1 )
+
+        self.play( AnimationGroup( FadeIn( howreversed ), FadeIn( howorig ) ) )
+        self.wait( 1 )
+        self.play( AnimationGroup( FadeOut( howreversed ), FadeOut( howorig ) ) )
+        self.wait( 2 )
+
+        self.wait( 14 )
+        equnit[0].shift( 2.5 * RIGHT + 0.2 * UP )
+        equnit[1].shift( 2.5 * RIGHT + 0.25 * DOWN )
+        equnit.set_color( RED )
+        self.play( ReplacementTransform( eqreversed, equnit ) )
+        self.wait( 8 )
 
 
 
