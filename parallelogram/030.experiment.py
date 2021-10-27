b4f892c74bd2a2d9e82c3b2c58a3dc2fa377d199
Peeter Joot <peeter.joot@lzlabs.com>
2021-10-26 23:19:43 -0400

    030.experiment.py    re-automate the latex generation.

--- parallelogram/030.experiment.py
+++ parallelogram/030.experiment.py
@@ -65,66 +65,64 @@ class foo( Scene ):
         #ar_0 = [ r'{{ \text{Area} }} {}^2', '=', r'{{ \text{base} }} {}^2 \times {{ \text{height} }} {}^2' ]
         ar_0 = [ l.sq( t_area ), '=', l.sq( t_base ), r' \times ', l.sq( t_height ) ]
 
-        ar_1 = [ '=', squ, l.norm2( l.sub( vecv, l.mult( l.lr( l.dot( vecv, hatu ) ), hatu ) ) ) ]
-        eq2 = [ '=', squ, l.lr( l.add( sqv, l.lrsq( l.dot( vecv, hatu ) ) ), l.neg( l.mult( '2', l.lrsq( l.dot( vecv, hatu ) ) ) ) ) ]
-        eq3 = [ '=', squ, l.lr( l.sub( sqv, l.lrsq( l.dot( vecv, hatu ) ) ) ) ]
-        eq4 = [ '=', l.sub( l.mult( squ, sqv ), l.mult( squ, l.lrsq( l.dot( vecv, hatu ) ) ) ) ]
-        eq5 = [ '=', l.sub( l.mult( squ, sqv ), l.lrsq( l.dot( vecv, l.lr( normu, hatu ) ) ) ) ]
-
         #ar_1  = [ '=',
         #          r'\lVert {{ \mathbf{u} }} \rVert {}^2 \lVert {{ \mathbf{v} }} - ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ) {{ \mathbf{\hat{u} } }} \rVert {}^2' ]
-        ar_2 = [ '=',
-                r' \lVert {{\mathbf{u} }} \rVert {}^2 \Bigl( \lVert {{ \mathbf{v} }} \rVert {}^2 + ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ) {}^2 - 2 ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ){}^2 \Bigr)' ]
-        ar_3 = [ '=',
-                r' \lVert {{\mathbf{u} }} \rVert {}^2 \Bigl( \lVert {{ \mathbf{v} }} \rVert {}^2 - ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ){}^2 \Bigr)' ]
+        ar_1 = [ '=', squ, l.norm2( l.sub( vecv, l.mult( l.lr( l.dot( vecv, hatu ) ), hatu ) ) ) ]
+        #ar_2 = [ '=',
+        #        r' \lVert {{\mathbf{u} }} \rVert {}^2 \Bigl( \lVert {{ \mathbf{v} }} \rVert {}^2 + ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ) {}^2 - 2 ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ){}^2 \Bigr)' ]
+        ar_2 = [ '=', squ, l.lr( l.add( sqv, l.lrsq( l.dot( vecv, hatu ) ) ), l.neg( l.mult( '2', l.lrsq( l.dot( vecv, hatu ) ) ) ), big = 1 ) ]
+        #ar_3 = [ '=',
+        #        r' \lVert {{\mathbf{u} }} \rVert {}^2 \Bigl( \lVert {{ \mathbf{v} }} \rVert {}^2 - ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ){}^2 \Bigr)' ]
+        ar_3 = [ '=', squ, l.lr( l.sub( sqv, l.lrsq( l.dot( vecv, hatu ) ) ), big = 1 ) ]
+        #ar_4 = [ '=',
+        #        r' \lVert {{ \mathbf{u} }} \rVert {}^2 \lVert {{ \mathbf{v} }} \rVert {}^2 - \lVert {{ \mathbf{u} }} \rVert {}^2 ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ){}^2' ]
+        ar_4 = [ '=', squ, sqv, '-', squ, l.lrsq( l.dot( vecv, hatu ) ) ]
 
-        ar_4 = [ '=',
-                r' \lVert {{ \mathbf{u} }} \rVert {}^2 \lVert {{ \mathbf{v} }} \rVert {}^2 - \lVert {{ \mathbf{u} }} \rVert {}^2 ( {{ \mathbf{v} }} \cdot {{ \mathbf{\hat{u} } }} ){}^2' ]
-        ar_5 = [ '=',
-                r' \lVert {{ \mathbf{u} }} \rVert {}^2 \lVert {{ \mathbf{v} }} \rVert {}^2 - \Bigl( {{ \mathbf{v} }} \cdot ( \lVert {{ \mathbf{u} }} \rVert {{ \mathbf{\hat{u} } }} ) \Bigr){}^2' ]
+        #ar_5 = [ '=',
+        #        r' \lVert {{ \mathbf{u} }} \rVert {}^2 \lVert {{ \mathbf{v} }} \rVert {}^2 - \Bigl( {{ \mathbf{v} }} \cdot ( \lVert {{ \mathbf{u} }} \rVert {{ \mathbf{\hat{u} } }} ) \Bigr){}^2' ]
+        #ar_5 = [ '=', l.sub( l.mult( squ, sqv ), l.lrsq( l.dot( vecv, l.lr( normu, hatu ) ) ) ) ]
+
         ar_6 = [ '=',
                 r' \lVert {{ \mathbf{u} }} \rVert {}^2 \lVert {{ \mathbf{v} }} \rVert {}^2 - ( {{ \mathbf{v} }} \cdot {{ \mathbf{u} }} ){}^2' ]
         #ar_6 = [ '=', l.sub( l.mult( squ, sqv ), l.lrsq( vdotu ) ) ]
 
-        eq_area_text = MathTex( *ar_a )
-        eq_area_text.set_color_by_tex_to_color_map( acolors )
-        eq_area_text.shift( 2 * UP )
+        eq_a = MathTex( *ar_a )
+        eq_a.set_color_by_tex_to_color_map( acolors )
+        eq_a.shift( 2 * UP )
 
-        eq_area_squared_text = MathTex( *ar_0 )
-        eq_area_squared_text.set_color_by_tex_to_color_map( acolors )
-        eq_area_squared_text.shift( 2 * UP )
+        eq_0 = MathTex( *ar_0 )
+        eq_0.set_color_by_tex_to_color_map( acolors )
+        eq_0.shift( 2 * UP )
 
-        eq_sq_norms = MathTex( *ar_1 )
+        eq_1 = MathTex( *ar_1 )
         eq_2 = MathTex( *ar_2 )
         eq_3 = MathTex( *ar_3 )
         eq_4 = MathTex( *ar_4 )
-        eq_5 = MathTex( *ar_5 )
+        #eq_5 = MathTex( *ar_5 )
         eq_6 = MathTex( *ar_6 )
 
-        self.play( Write( eq_area_text ) )
+        self.play( Write( eq_a ) )
         self.wait( )
-        self.play( TransformMatchingTex( eq_area_text, eq_area_squared_text ) )
+        self.play( TransformMatchingTex( eq_a, eq_0 ) )
         self.wait( )
-        write_aligned( self, eq_area_squared_text, eq_sq_norms, 0.75 * DOWN, uvcolors )
+        write_aligned( self, eq_0, eq_1, 0.75 * DOWN, uvcolors )
         self.wait( )
-        write_aligned( self, eq_sq_norms, eq_2, 0.75 * DOWN, uvcolors )
+        write_aligned( self, eq_1, eq_2, 0.75 * DOWN, uvcolors )
         self.wait( )
 
         eq_3.move_to( eq_2, LEFT )
         eq_3.set_color_by_tex_to_color_map( uvcolors )
         eq_4.move_to( eq_3, LEFT )
         eq_4.set_color_by_tex_to_color_map( uvcolors )
-        eq_5.move_to( eq_4, LEFT )
-        eq_5.set_color_by_tex_to_color_map( uvcolors )
-        eq_6.move_to( eq_5, LEFT )
+        #eq_5.move_to( eq_4, LEFT )
+        #eq_5.set_color_by_tex_to_color_map( uvcolors )
+        eq_6.move_to( eq_4, LEFT )
         eq_6.set_color_by_tex_to_color_map( uvcolors )
         self.play( TransformMatchingTex( eq_2, eq_3 ) )
         self.wait( )
         self.play( TransformMatchingTex( eq_3, eq_4 ) )
         self.wait( )
-        self.play( TransformMatchingTex( eq_4, eq_5 ) )
-        self.wait( )
-        self.play( TransformMatchingTex( eq_5, eq_6 ) )
+        self.play( TransformMatchingTex( eq_4, eq_6 ) )
         self.wait( )
 
 
