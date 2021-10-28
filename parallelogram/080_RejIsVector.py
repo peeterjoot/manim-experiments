5c244acb346e72eb24fc66aca60b3209fded0801
Peeter Joot <peeter.joot@lzlabs.com>
2021-10-27 23:35:30 -0400

    080_RejIsVector.py    colorize.

--- parallelogram/080_RejIsVector.py
+++ parallelogram/080_RejIsVector.py
@@ -1,37 +1,51 @@
 from helper import *
+from helper2 import *
 
 class RejIsVector( Scene ):
     def construct( self ):
         myTemplate = TexTemplate( )
         myTemplate.add_to_preamble( r'\usepackage{cancel}' )
 
-        eq = MathTex(
-                concat( l.Rej( vecu, vecv ), r' &= ', lr_vwedgeu, invu, l.newline ),
-                concat( r'&= ', l.add( l.dot( lr_vwedgeu, invu ), l.wedge( lr_vwedgeu, invu ) ), l.newline ),
-                concat( r'&= ', l.dot( lr_vwedgeu, invu ), ' + ', vecv, r'\wedge' ), concat( l.lr( l.wedge( vecu, invu ) ), l.newline ),
-                concat( r'&= ', l.dot( lr_vwedgeu, invu ), l.newline ),
-                concat( r'&= ', vecv, ' - ', l.lr( l.dot( vecv, vecu ) ), invu ) )
+        eq1 = MathTex( l.Rej( vecu, vecv ), '=', lr_vwedgeu, invu )
+        eq2a = MathTex( '=', l.dot( lr_vwedgeu, invu ), '+' )
+        eq2b = MathTex( l.wedge( lr_vwedgeu, invu ) )
 
-        cancel = MathTex( l.lr( l.cancel( l.wedge( vecu, invu ) ) ), tex_template = myTemplate )
+        # just the trivector term:
+        eq3a = MathTex( vecv, r'\wedge' )
+        eq3b = MathTex( l.lr( l.wedge( vecu, invu ), big = 1 ) )
+        eq4 = MathTex( '=', l.dot( lr_vwedgeu, invu ) )
+        eq5 = MathTex( '=', vecv, ' - ', l.lr( l.dot( vecv, vecu ) ), invu )
+        eq1.set_color_by_tex_to_color_map( acolors )
+        eq1.shift( 2 * UP + 2 * LEFT )
 
-        for i in range(2):
-           self.play( Write( eq[i] ) )
-           self.wait( )
-        i = 2
-        self.play( AnimationGroup( Write( eq[i] ), Write( eq[i+1] ) ) )
+        self.play( Write( eq1 ) )
         self.wait( )
-        cancel.move_to( eq[i+1] )
-
-        self.play( ReplacementTransform( eq[i+1], cancel ) )
+        g = VGroup( eq2a, eq2b )
+        where = eq1.get_part_by_tex( '=' )
+        eq2a.move_to( where, LEFT )
+        eq2a.shift( 1.25 * DOWN )
+        eq2a.set_color_by_tex_to_color_map( acolors )
+        eq2b.next_to( eq2a, RIGHT )
+        eq2b.set_color_by_tex_to_color_map( acolors )
+        self.play( AnimationGroup( Write( eq2a ), Write( eq2b ) ) )
         self.wait( )
 
-        i += 2
-        self.play( Write( eq[i] ) )
+        eq3a.set_color_by_tex_to_color_map( acolors )
+        eq3b.set_color_by_tex_to_color_map( acolors )
+        eq3a.move_to( eq2b, LEFT )
+        eq3b.next_to( eq3a, RIGHT )
+        self.play( ReplacementTransform( eq2b, VGroup( eq3a, eq3b ) ) )
+        self.wait( )
+        self.play( Indicate( eq3b ) )
         self.wait( )
 
-        i += 1
-        self.play( Write( eq[i] ) )
+        write_aligned( self, eq2a, eq4, 1.25 * DOWN, acolors )
         self.wait( )
+
+        write_aligned( self, eq4, eq5, 1.25 * DOWN, acolors )
+        self.wait( )
+
+        #cancel = MathTex( l.lr( l.cancel( l.wedge( vecu, invu ) ) ), tex_template = myTemplate )
 
 
 
