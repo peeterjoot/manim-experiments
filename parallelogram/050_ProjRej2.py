1d6705b558bebe405eb6706595ea2c6ec4fff86f
Peeter Joot <peeter.joot@lzlabs.com>
2021-10-30 12:07:17 -0400

    modified:   050_ProjRej2.py modified:   050_ProjRej2.script.txt
       tweak when the v = v expressions are shown, and add reversed uhat
       rejection.

--- parallelogram/050_ProjRej2.py
+++ parallelogram/050_ProjRej2.py
@@ -4,21 +4,29 @@ class ProjRej2( Scene ):
     def construct( self ):
         proj       = l.mult( lr_vdotu, invu )
         rej        = l.mult( lr_vwedgeu, invu )
-        proj2      = l.mult( l.lr( l.dot( vecv, hatu ) ), hatu )
-        rej2       = l.mult( l.lr( l.wedge( vecv, hatu ) ), hatu )
         projr      = l.mult( invu, lr_udotv )
         rejr       = l.mult( invu, lr_uwedgev )
+
+        howorig = MathTex( concat( vecv, ' = ', vecv, vecu, invu ) )
+        howorig.set_color_by_tex_to_color_map( acolors )
+        howorig.move_to( 2 * UP + 1.0 * LEFT )
+
+        howreversed = MathTex( concat( vecv, ' = ', invu, vecu, vecv ) )
+        howreversed.set_color_by_tex_to_color_map( acolors )
+        howreversed.move_to( 2 * UP + 1.8 * RIGHT )
+
 
         eqa = MathTex( l.Proj( vecu, vecv ), ' \equiv ', proj )
         eqa.set_color_by_tex_to_color_map( acolors )
         eqa.shift( 2 * LEFT )
-        self.play( Write( eqa ) )
+        self.play( AnimationGroup( FadeIn( howorig ), Write( eqa ) ) )
 
         eqb = MathTex( l.Rej( vecu, vecv ),  ' \equiv ', rej )
         write_aligned( self, eqa, eqb, 1.25 * DOWN, acolors, 'Proj' )
-        self.wait(3)
+
+        #self.wait(3)
         # +7s
-        self.wait(2)
+        #self.wait(2)
         # +7s
 
         eqreversed1 = MathTex( concat( '=', projr ) )
@@ -30,33 +38,30 @@ class ProjRej2( Scene ):
         eqreversed2.next_to( eqb, RIGHT );
         eqreversed2.set_color_by_tex_to_color_map( acolors )
 
-        self.play( AnimationGroup( Write( eqreversed1 ), Write( eqreversed2 ) ) )
+        self.play( AnimationGroup( FadeIn( howreversed ), Write( eqreversed1 ), Write( eqreversed2 ) ) )
         self.wait( 5 )
 
-        howorig = MathTex( concat( vecv, ' = ', vecv, vecu, invu ) )
-        howorig.set_color_by_tex_to_color_map( acolors )
-        howorig.move_to( 2 * UP + 0.2 * LEFT )
-
-        howreversed = MathTex( concat( vecv, ' = ', invu, vecu, vecv ) )
-        howreversed.set_color_by_tex_to_color_map( acolors )
-        howreversed.move_to( 2 * UP + 2.8 * RIGHT )
-
-        self.play( AnimationGroup( FadeIn( howreversed ), FadeIn( howorig ) ) )
-        self.wait( 1 )
         self.play( AnimationGroup( FadeOut( howreversed ), FadeOut( howorig ) ) )
-        self.wait( 1 )
+        self.wait( )
 
-
+        proj2      = l.mult( l.lr( l.dot( vecv, hatu ) ), hatu )
+        rej2       = l.mult( l.lr( l.wedge( vecv, hatu ) ), hatu )
+        rej2r      = l.mult( hatu, l.lr( l.wedge( hatu, vecv ) ) )
         equnit1 = MathTex( concat( '=', proj2 ) )
         equnit2 = MathTex( concat( '=', rej2 ) )
+        equnit2r = MathTex( concat( '=', rej2r ) )
         self.wait( 14 )
         equnit1.next_to( eqreversed1, RIGHT )
         equnit2.next_to( eqreversed2, RIGHT )
+        equnit2r.next_to( eqreversed2, RIGHT )
         equnit1.set_color_by_tex_to_color_map( acolors )
         equnit2.set_color_by_tex_to_color_map( acolors )
+        equnit2r.set_color_by_tex_to_color_map( acolors )
         self.play( AnimationGroup( Write( equnit1 ), Write( equnit2 ) ) )
+        self.wait( 1 )
+        self.play( TransformMatchingTex( equnit2, equnit2r ) )
+        self.wait( 7 )
         #self.play( ReplacementTransform( eqreversed, equnit ) )
-        self.wait( 8 )
 
 
 
