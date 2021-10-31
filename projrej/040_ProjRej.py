85b720dc16f9faa848bc5e71e6e3f93a1f773406
Peeter Joot <peeter.joot@lzlabs.com>
2021-10-31 14:00:55 -0400

    modified:   040_ProjRej.py modified:   040_ProjRej.script.txt
       explicitly show both decompositions.

--- projrej/040_ProjRej.py
+++ projrej/040_ProjRej.py
@@ -3,11 +3,27 @@ from helper import *
 class ProjRej( Scene ):
     def construct( self ):
 
-        proj       = l.mult( lr_vdotu, invu )
-        rej        = l.mult( lr_vwedgeu, invu )
-
         labels = DrawVectorsAndProjRej( self, 0 )
 
+        projr       = l.mult( invu, lr_udotv )
+        rejr        = l.mult( invu, lr_uwedgev )
+        eq2 = MathTex( concat( vecv, r' &= 1 \times ', vecv, l.newline ),
+                       concat( r' &= ', l.lr( invu, vecu, big = 1 ), vecv, l.newline ),
+                       concat( r' &= ', invu, l.lr( vecu, vecv ), l.newline ),
+                       concat( r' &= ', invu, l.lr( l.add( udotv, uwedgev ) ), l.newline ),
+                       concat( r' &= ', l.add( projr, rejr ), l.newline ) )
+        eq2.set_color_by_tex_to_color_map( acolors )
+
+        eq2.shift( 2 * RIGHT )
+        for item in eq2:
+           self.play( Write( item ) )
+           self.wait( 7 )
+        self.wait( 1 )
+        self.play( FadeOut( eq2 ) )
+
+
+        proj       = l.mult( lr_vdotu, invu )
+        rej        = l.mult( lr_vwedgeu, invu )
         eq = MathTex( concat( vecv, r' &= ', vecv, r'\times 1', l.newline ),
                       concat( r' &= ', vecv, l.lr( vecu, invu, big = 1 ), l.newline ),
                       concat( r' &= ', l.lr( vecv, vecu ), invu, l.newline ),
@@ -18,8 +34,10 @@ class ProjRej( Scene ):
         eq.shift( 2 * RIGHT )
         for item in eq:
            self.play( Write( item ) )
-           self.wait( 7 )
+           self.wait( 1 )
         self.wait( 1 )
+
+
 
         o = np.array( [ -4, -2, 0 ] )
         p1 = dv_p1
