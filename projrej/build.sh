af0af4ce61f6012e425f6431e15e7172b5e7eaa3
Peeter Joot <peeter.joot@lzlabs.com>
2022-04-21 19:56:43 -0400

    new file:   build.sh

---
+++ projrej/build.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+q='-q h'
+
+#../bin/build $q -p 010_TitlePage.py
+#../bin/build $q -p 020_Overview.py
+#../bin/build $q -p 040_ProjRej.py
+#../bin/build $q -p 045_ProjRejGraphical.py
+#../bin/build $q -p 050_ProjRej2.py
+#../bin/build $q -p 051_Inverse.py
+#../bin/build $q -p 052_ProjParallelToU.py
+#../bin/build $q -p 070_ProjRejPerp.py
+#../bin/build $q -p 080_RejIsVector.py
+##../bin/build $q -p 090_RejR3.py
+##../bin/build $q -p 091_RejCross.py # skipping.
+##../bin/build $q -p 092_Rejvisualization.py
+##../bin/build $q -p 093_RejGAVisualization.py
+../bin/build $q -p 130_SummaryTable.py
+#../bin/build $q -p 150_Finale.py
