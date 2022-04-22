5a020f162659b33ba3b410eb5c17070927f1a0bb
Peeter Joot <peeter.joot@lzlabs.com>
2022-04-22 10:52:57 -0400

    modified:   150_Finale.py    tweak the final scene text (in particular: peeterjoot.com/gaee short    link).

--- projrej/150_Finale.py
+++ projrej/150_Finale.py
@@ -2,23 +2,28 @@ from manim import *
 
 class Finale( Scene ):
     def construct( self ):
-        t = Text( 'Special thanks to' )
+        t = Text( 'Special thanks to: ' )
         t.move_to( 2.5 * UP + 4 * LEFT )
         t2 = Text( 'sudgylacmoe' )
-        t2.next_to( t, DOWN )
-        t3 = Text( 'Who generously hand held me through my first Manim baby steps.' ).scale( 0.6 )
-        t3.next_to( t2, DOWN )
-        t3.shift( 3 * RIGHT )
+        t2.next_to( t, RIGHT )
+        t3 = Text( '(Who generously hand held me through my first Manim baby steps.)' ).scale( 0.6 )
+        t3.next_to( t, DOWN )
+        t3.shift( 4 * RIGHT )
 
-        t4a = Text( 'Check out:' )
+        t4a = Text( 'My book: ' )
+
         t4a.set_color( TEAL )
-        t4 = Tex("\\verb|http://peeterjoot.com/writing/geometric-algebra-for-electrical-engineers/|").scale( 0.7 )
-        t4a.next_to( t3, DOWN )
-        t4a.shift( 4 * LEFT )
-        t4.next_to( t4a, DOWN )
-        t4.shift( 5 * RIGHT )
+        t4a.next_to( t, DOWN )
+        t4a.shift( 1 * DOWN + 1.0 * LEFT )
 
-        g = VGroup( t, t2, t3, t4a, t4 )
+        t4b = Text( 'Geometric Algebra for Electrical Engineers' ).scale( 0.8 )
+        t4b.next_to( t4a, RIGHT )
+
+        t4 = Tex("\\verb|http://peeterjoot.com/gaee/|") #.scale( 0.7 )
+        t4.next_to( t4a, DOWN )
+        t4.shift( 4 * RIGHT )
+
+        g = VGroup( t, t2, t3, t4, t4a, t4b )
         t.set_color( TEAL )
         self.add( g )
 
