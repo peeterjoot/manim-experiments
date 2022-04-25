a33e3ec37deae2edb8cb2c5b09ddf0c100b04e95
Peeter Joot <peeter.joot@lzlabs.com>
2022-04-25 12:24:42 -0400

    new file:   Coordinates_005.py modified:   Position_010.py    split into two.

---
+++ circular/Coordinates_005.py
@@ -0,0 +1,119 @@
+from helper import *
+
+class Coordinates_005( Scene ):
+    def construct( self ):
+
+        title = Text( 'Circular coordinates.' )
+        title.move_to( 3 * UP )
+        title.set_color( BLUE )
+        self.add( title )
+        self.wait( 5 )
+
+        axes = Axes( x_range = [-1, 4, 1], y_range = [-1, 4, 1], x_length = 4, y_length = 4,
+                     axis_config = {"include_tip": True, "numbers_to_exclude": [0]} ).add_coordinates()
+        axes.to_edge( LEFT )
+
+        rline = NumberLine(
+            x_range=[0, 4, 1],
+            length=4,
+            color=BLUE,
+            include_numbers=True,
+            label_direction=DOWN,
+        )
+        rline.to_edge( RIGHT )
+        rtex = MathTex( 'r' )
+        rtex.move_to( rline, LEFT )
+        rtex.shift( 0.5 * LEFT )
+
+        tline = NumberLine(
+            x_range=[ 0, 4 ],
+            length=4,
+            color=BLUE,
+            include_numbers=False,
+            label_direction=DOWN,
+        )
+        tline.add_labels( {
+            0: MathTex('0'),
+            1: MathTex(r'\pi/4'),
+            2: MathTex(r'\pi/2'),
+            3: MathTex(r'3\pi/4'),
+            4: MathTex(r'\pi/2')
+            } )
+        tline.move_to( rline, DOWN )
+        tline.shift( 1.0 * DOWN )
+        ttex = MathTex( r'\theta' )
+        ttex.move_to( tline, LEFT )
+        ttex.shift( 0.5 * LEFT )
+
+        g1 = ParametricFunction( lambda t: axes.coords_to_point(np.cos(t * 0.25 * PI), np.sin(t * 0.25 * PI)),
+                                 t_range=[0, 1],
+                                 scaling=axes.x_axis.scaling, color=YELLOW )
+
+        r1p = ValueTracker(0)
+        r1u = lambda mob: mob.move_to(rline.number_to_point(1 + r1p.get_value()))
+        r1d = Dot(color=RED).add_updater(r1u).update()
+
+        t1p = ValueTracker(0)
+        t1u = lambda mob: mob.move_to(tline.number_to_point(2 * t1p.get_value()))
+        t1d = Dot(color=YELLOW).add_updater(t1u).update()
+
+        self.play( DrawBorderThenFill( VGroup( axes, r1d, rline, rtex, t1d, tline, ttex ) ), run_time = 2 )
+        self.wait( )
+
+        self.play( AnimationGroup( Create( g1, run_time=6 ),
+                                   UpdateFromAlphaFunc( t1p, 
+                                                        lambda mob, alpha: mob.set_value(alpha),
+                                                        run_time=6 ),
+                                   ) )
+        self.wait( 4 )
+
+        p = np.array( [ np.cos( 0.25 * PI ), np.sin( 0.25 * PI ) ] )
+        g2 = ParametricFunction( lambda t: axes.coords_to_point( (t+1) * p[0], (t+1) * p[1] ),
+                                 t_range=[0, 1],
+                                 scaling=axes.x_axis.scaling, color=RED )
+
+        self.play( AnimationGroup( Create( g2, run_time=6 ),
+                                   UpdateFromAlphaFunc( r1p, 
+                                                        lambda mob, alpha: mob.set_value(alpha),
+                                                        run_time=6 ),
+                                   ) )
+        self.wait( 4 )
+
+        def radius(x, y):
+            r = np.sqrt( x*x + y*y )
+            return r
+
+        def angle(x, y):
+            a = np.arctan2( y, x )
+            return a
+
+        p2 = 2 * p
+        f = lambda t: axes.coords_to_point( p2[0] + np.sin(10 * t), p2[1] + 1.0 * (np.exp(t)-1) )
+        g3 = ParametricFunction( f,
+                                 t_range=[0, 1],
+                                 scaling=axes.x_axis.scaling, color=GREEN )
+
+        # PI/4 => 2
+        # PI   => 8
+        # 2 PI => 16
+        t3a = lambda t: (8 / math.pi) * angle( p2[0] + np.sin(10 * t), p2[1] + 1.0 * ( np.exp(t)-1 ) )
+        t3r = lambda t: radius( p2[0] + np.sin(10 * t), p2[1] + 1.0 * ( np.exp(t)-1 ) )
+        t3u = lambda mob: mob.move_to(tline.number_to_point(t3a(t1p.get_value())))
+        r3u = lambda mob: mob.move_to(rline.number_to_point(t3r(r1p.get_value())))
+        t1d.remove_updater(t1u)
+        r1d.remove_updater(r1u)
+        t1d.add_updater(t3u).update()
+        r1d.add_updater(r3u).update()
+
+
+        self.play( AnimationGroup( Create( g3, run_time=6 ),
+                                   UpdateFromAlphaFunc( r1p, 
+                                                        lambda mob, alpha: mob.set_value(alpha),
+                                                        run_time=6 ),
+                                   UpdateFromAlphaFunc( t1p, 
+                                                        lambda mob, alpha: mob.set_value(alpha),
+                                                        run_time=6 ),
+                                   ) )
+        self.wait( 4 )
+
+# vim: et sw=4 ts=4
--- circular/Position_010.py
+++ circular/Position_010.py
@@ -3,194 +3,38 @@ from helper import *
 class Position_010( Scene ):
     def construct( self ):
 
-        #title = Text( 'Circular velocity and acceleration.' )
         title = Text( 'Circular coordinates.' )
         title.move_to( 3 * UP )
         title.set_color( BLUE )
         self.add( title )
         self.wait( 5 )
 
-        if 0:
-            eq = [ AcolorsMathTex( concat( vec_x, ' = x_r ', hat_r, r' + x_\theta', hat_theta ) ),
-                   AcolorsMathTex( concat( hat_r, ' = ', vec_e1, r'e^{i\theta}' ) ),
-                   AcolorsMathTex( concat( hat_theta, ' = ', vec_e2, r'e^{i\theta}' ) ),
-                   AcolorsMathTex( concat( 'i = ', vec_e1, vec_e2 ) ),
-                   AcolorsMathTex( concat( vec_e1, vec_e1, ' = ', vec_e2, vec_e2, '= 1' ) ),
-                   AcolorsMathTex( concat( vec_e1, vec_e2, ' = -', vec_e2, vec_e1 ) ) ]
+        eq = [ AcolorsMathTex( concat( vec_x, ' = x_r ', hat_r, r' + x_\theta', hat_theta ) ),
+               AcolorsMathTex( concat( hat_r, ' = ', vec_e1, r'e^{i\theta}' ) ),
+               AcolorsMathTex( concat( hat_theta, ' = ', vec_e2, r'e^{i\theta}' ) ),
+               AcolorsMathTex( concat( 'i = ', vec_e1, vec_e2 ) ),
+               AcolorsMathTex( concat( vec_e1, vec_e1, ' = ', vec_e2, vec_e2, '= 1' ) ),
+               AcolorsMathTex( concat( vec_e1, vec_e2, ' = -', vec_e2, vec_e1 ) ) ]
 
-            eq[0].shift( 1.5 * UP )
-            self.play( Write( eq[0] ) )
-            self.wait( 5 )
+        eq[0].shift( 1.5 * UP )
+        self.play( Write( eq[0] ) )
+        self.wait( 5 )
 
-            i = 0
-            write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.40 * LEFT, None )
-            self.wait( 5 )
-            i = 1
-            write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.40 * LEFT, None )
-            self.wait( 5 )
-            i = 2
-            write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.35 * LEFT, None )
-            self.wait( 5 )
-            i = 3
-            write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.80 * LEFT, None )
-            self.wait( 5 )
-            i = 4
-            write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.00 * LEFT, None )
-            self.wait( 5 )
+        i = 0
+        write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.40 * LEFT, None )
+        self.wait( 5 )
+        i = 1
+        write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.40 * LEFT, None )
+        self.wait( 5 )
+        i = 2
+        write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.35 * LEFT, None )
+        self.wait( 5 )
+        i = 3
+        write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.80 * LEFT, None )
+        self.wait( 5 )
+        i = 4
+        write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.00 * LEFT, None )
+        self.wait( 5 )
 
-
-
-        axes = Axes( x_range = [-1, 4, 1], y_range = [-1, 4, 1], x_length = 4, y_length = 4,
-                     axis_config = {"include_tip": True, "numbers_to_exclude": [0]} ).add_coordinates()
-        axes.to_edge( DOWN + LEFT )
-
-        function = lambda theta: 2 * np.sin(theta * 5)
-
-        rline = NumberLine(
-            x_range=[0, 4, 1],
-            length=4,
-            color=BLUE,
-            include_numbers=True,
-            label_direction=DOWN,
-        )
-        rline.shift( 4 * LEFT + 2.5 * UP )
-        rtex = MathTex( 'r' )
-        rtex.move_to( rline, LEFT )
-        rtex.shift( 0.5 * LEFT )
-
-        tline = NumberLine(
-            x_range=[ 0, 4 ],
-            length=4,
-            color=BLUE,
-            include_numbers=False,
-            label_direction=DOWN,
-        )
-        tline.add_labels( {
-            0: MathTex('0'),
-            1: MathTex(r'\pi/4'),
-            2: MathTex(r'\pi/2'),
-            3: MathTex(r'3\pi/4'),
-            4: MathTex(r'\pi/2')
-            } )
-        tline.move_to( rline, DOWN )
-        tline.shift( 1.0 * DOWN )
-        ttex = MathTex( r'\theta' )
-        ttex.move_to( tline, LEFT )
-        ttex.shift( 0.5 * LEFT )
-
-        g1 = ParametricFunction( lambda t: axes.coords_to_point(np.cos(t * 0.25 * PI), np.sin(t * 0.25 * PI)),
-                                 t_range=[0, 1],
-                                 scaling=axes.x_axis.scaling, color=YELLOW )
-
-        r1p = ValueTracker(0)
-        r1u = lambda mob: mob.move_to(rline.number_to_point(1 + r1p.get_value()))
-        r1d = Dot(color=RED).add_updater(r1u).update()
-
-        t1p = ValueTracker(0)
-        t1u = lambda mob: mob.move_to(tline.number_to_point(2 * t1p.get_value()))
-        t1d = Dot(color=YELLOW).add_updater(t1u).update()
-
-        self.play( DrawBorderThenFill( VGroup( axes, r1d, rline, rtex, t1d, tline, ttex ) ), run_time = 2 )
-        self.wait( )
-
-        self.play( AnimationGroup( Create( g1, run_time=6 ),
-                                   UpdateFromAlphaFunc( t1p, 
-                                                        lambda mob, alpha: mob.set_value(alpha),
-                                                        run_time=6 ),
-                                   ) )
-        self.wait( 4 )
-
-        p = np.array( [ np.cos( 0.25 * PI ), np.sin( 0.25 * PI ) ] )
-        g2 = ParametricFunction( lambda t: axes.coords_to_point( (t+1) * p[0], (t+1) * p[1] ),
-                                 t_range=[0, 1],
-                                 scaling=axes.x_axis.scaling, color=RED )
-
-        self.play( AnimationGroup( Create( g2, run_time=6 ),
-                                   UpdateFromAlphaFunc( r1p, 
-                                                        lambda mob, alpha: mob.set_value(alpha),
-                                                        run_time=6 ),
-                                   ) )
-        self.wait( 4 )
-
-        def radius(x, y):
-            r = np.sqrt( x*x + y*y )
-            return r
-
-        def angle(x, y):
-            a = np.arctan2( y, x )
-            return a
-
-        p2 = 2 * p
-        f = lambda t: axes.coords_to_point( p2[0] + np.sin(10 * t), p2[1] + 1.0 * (np.exp(t)-1) )
-        g3 = ParametricFunction( f,
-                                 t_range=[0, 1],
-                                 scaling=axes.x_axis.scaling, color=GREEN )
-
-        # PI/4 => 2
-        # PI   => 8
-        # 2 PI => 16
-        t3a = lambda t: (8 / math.pi) * angle( p2[0] + np.sin(10 * t), p2[1] + 1.0 * ( np.exp(t)-1 ) )
-        t3r = lambda t: radius( p2[0] + np.sin(10 * t), p2[1] + 1.0 * ( np.exp(t)-1 ) )
-        t3u = lambda mob: mob.move_to(tline.number_to_point(t3a(t1p.get_value())))
-        r3u = lambda mob: mob.move_to(rline.number_to_point(t3r(r1p.get_value())))
-        t1d.remove_updater(t1u)
-        r1d.remove_updater(r1u)
-        t1d.add_updater(t3u).update()
-        r1d.add_updater(r3u).update()
-
-
-        self.play( AnimationGroup( Create( g3, run_time=6 ),
-                                   UpdateFromAlphaFunc( r1p, 
-                                                        lambda mob, alpha: mob.set_value(alpha),
-                                                        run_time=6 ),
-                                   UpdateFromAlphaFunc( t1p, 
-                                                        lambda mob, alpha: mob.set_value(alpha),
-                                                        run_time=6 ),
-                                   ) )
-        self.wait( 4 )
-
-
-
-        if 0:
-
-
-            #self.play( DrawBorderThenFill( VGroup( axes, rline, rtex, tline, ttex ) ), run_time = 2 )
-            self.play( DrawBorderThenFill( VGroup( tline, ttex ) ), run_time = 2 )
-            self.wait( )
-
-            self.play( AnimationGroup( #Create(g1),
-                                       UpdateFromAlphaFunc( t1p, 
-                                                            lambda mob, alpha: mob.set_value(alpha * PI/2),
-                                                            run_time=6 ),
-                                       #UpdateFromAlphaFunc( r1p, 
-                                       #                     lambda mob, alpha: mob.set_value(1),
-                                       #                     run_time=6 )
-                                       ) )
-            self.wait( )
-
-            #self.play( DrawBorderThenFill( VGroup( axes, rline, rtex, tline, ttex ) ), run_time = 2 )
-            #self.play( DrawBorderThenFill( VGroup( tline, ttex ) ), run_time = 2 )
-            #self.wait( )
-            #self.play( AnimationGroup(Create(t2), Create(r2), Create(g2)), run_time = 6 )
-            #self.wait( )
-            #self.play( AnimationGroup(Create(g3), Create(r3), Create(t3)), run_time = 6 )
-            #self.wait( )
-
-
-        if 0:
-
-            graphs = VGroup( g1, g2, g3 )
-
-            #r = lambda u: np.array([ np.cos(u), np.sin(u), 0 ])
-            #graph = ParametricFunction( r, color=RED, t_range=[0, 0.5 * PI] )
-            #graph.underlying_function = r
-
-            #graphing_stuff = VGroup( axes, g1, g2 )
-            #graphing_stuff += axis_labels
-
-            #self.play(DrawBorderThenFill(axes), Write(axis_labels), run_time = 2)
-            self.wait()
-            self.play(Create(graphs), run_time = 6)
-            self.wait()
 
 # vim: et sw=4 ts=4
