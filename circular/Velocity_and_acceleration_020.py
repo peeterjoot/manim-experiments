2f54662701c161e8174bfd9b31e5bb660e5ae072
Peeter Joot <peeter.joot@lzlabs.com>
2022-04-28 11:46:12 -0400

    Velocity_and_acceleration_020: summary transform

--- circular/Velocity_and_acceleration_020.py
+++ circular/Velocity_and_acceleration_020.py
@@ -45,5 +45,25 @@ class Velocity_and_acceleration_020( Scene ):
         write_aligned( self, eq2[i], eq2[i+1], 0.75 * DOWN, None )
         self.wait( 5 )
 
+        eq3 = AcolorsMathTex( concat(        " = ", l.lr( dr_prime, r" - r \omega^2" ), hat_r, '+', l.lr( r"2 r' \omega + r \omega'"), hat_theta ) )
+        eq3.move_to( eq2[i+1] )
+        self.play( TransformMatchingTex( eq2[i+1], eq3 ) )
+        self.wait( 5 )
+
+        eq4 = AcolorsMathTex( concat(        " = ", l.lr( dr_prime, r" - r \omega^2" ), hat_r, '+', l.frac('1', 'r'), prime( l.lr( r'r^2 \omega' ) ), hat_theta ) )
+        eq4.move_to( eq3 )
+        eq4.shift( 0.3 * DOWN + 0.38 * LEFT )
+        self.play( TransformMatchingTex( eq3, eq4 ) )
+        self.wait( 5 )
+
+        eq5 = AcolorsMathTex( concat( vec_v, " = ", r_prime, hat_r, r' + r \omega', hat_theta, r',\quad',
+            vec_a,
+            " = ", l.lr( dr_prime, r" - r \omega^2" ), hat_r, '+', l.frac('1', 'r'), prime( l.lr( r'r^2 \omega' ) ), hat_theta ) )
+
+        all = VGroup( *eq, eq2[0], eq2[1], eq2[2], eq2[3], eq4 )
+        self.play( TransformMatchingTex( all, eq5 ) )
+        self.wait( 5 )
+
+
 
 # vim: et sw=4 ts=4
