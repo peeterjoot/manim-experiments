3dda2883f33dc0d9b32915cea6a62d879e4b4f77
Peeter Joot <peeter.joot@lzlabs.com>
2022-04-29 00:13:46 -0400

    modified:   Circular_015.py

--- circular/Circular_015.py
+++ circular/Circular_015.py
@@ -6,90 +6,91 @@ class Circular_015( Scene ):
         title.move_to( 3 * UP )
         title.set_color( BLUE )
         self.play( FadeIn( title ) )
-        #self.wait( 5 )
+        self.wait( 1 )
 
-#        radius = 4
-#        axes = Axes( x_range = [0, 1, 1], y_range = [0, 1, 1], x_length = radius, y_length = radius,
-#                     axis_config = {"include_tip": True, "numbers_to_exclude": [0]} ).add_coordinates()
-#
-#        axes.shift( DOWN + 3 * LEFT )
-#        origin = axes.coords_to_point( 0, 0 )
-#        e1dir = RIGHT
-#        e2dir = UP
-#
-#        x1 = origin + radius * e1dir
-#        x2 = origin + radius * e2dir
-#
-#        r = lambda t: origin + radius * e1dir * np.cos( t * 0.5 * PI ) + radius * e2dir * np.sin( t * 0.5 * PI )
-#        tv = lambda t: - e1dir * np.sin( t * 0.5 * PI ) + e2dir * np.cos( t * 0.5 * PI )
-#
-#        def rmove(a, t):
-#            p = r(t)
-#            d = (p - origin)/radius
-#            a.put_start_and_end_on( p, p + d )
-#
-#        def tmove(a, t):
-#            p = r(t)
-#            d = tv(t)
-#            a.put_start_and_end_on( p, p + d )
-#
-#        def u1(mob):
-#            t = t_parameter.get_value()
-#            rmove( mob, t )
-#
-#        def u2(mob):
-#            t = t_parameter.get_value()
-#            tmove( mob, t )
-#
-#        def ur(mob):
-#            t = t_parameter.get_value()
-#            p = r(t)
-#            d = 1.25 * (p - origin)/radius
-#            mob.move_to( p + d )
-#
-#        def ut(mob):
-#            t = t_parameter.get_value()
-#            p = r(t)
-#            d2 = tv(t)
-#            d1 = 0.35 * (p - origin)/radius
-#            mob.move_to( p + d1 + d2 )
-#
-#        def ul(mob):
-#            t = t_parameter.get_value()
-#            p = r(t)
-#            mob.put_start_and_end_on( origin, p )
-#
-#        t_parameter = ValueTracker(0)
-#        e1 = Arrow( start = x1, end = x1 + e1dir, color = GREEN, buff = 0 ).add_updater(u1).update()
-#        e2 = Arrow( start = x1, end = x1 + e2dir, color = GREEN, buff = 0 ).add_updater(u2).update()
-#        line = Line( start = origin, end = x1, color = RED, buff = 0 ).add_updater(ul).update()
-#        rtex = AcolorsMathTex( hat_r ).add_updater(ur).update()
-#        ttex = AcolorsMathTex( hat_theta ).add_updater(ut).update()
-#
-#        eq = [ AcolorsMathTex( hat_r, ' = ', vec_e1, r'e^{i\theta}' ),
-#               AcolorsMathTex( hat_theta, ' = ', hat_r, ' i' ),
-#               AcolorsMathTex( 'i = ', vec_e1, vec_e2 ) ]
-#        eq[0].move_to( 2 * RIGHT )
-#
-#        g1 = ParametricFunction( r,
-#                                 t_range=[0, 1],
-#                                 scaling=axes.x_axis.scaling, color=YELLOW )
-#
-#        self.add( VGroup( axes, g1, e1, e2, rtex, ttex, line ) )
-#        self.wait( 1 )
-#
-#        self.play( Write( eq[0] ) )
-#        i = 0
-#        # Fixme: write_aligned works for aligning to ' = ', but only when it starts a line.
-#        write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.45 * LEFT, None )
-#        self.wait( 1 )
-#        i = 1
-#        write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.33 * LEFT, None )
-#        self.wait( 1 )
-#
-#        self.play( UpdateFromAlphaFunc( t_parameter, 
-#                                        lambda mob, alpha: mob.set_value( alpha ) ),
-#                                        run_time=6 )
-#        self.wait( 1 )
+        eq = AcolorsMathTex( concat( vec_v, " = ", r_prime, hat_r, r' + r \omega', hat_theta, r',\quad',
+            vec_a,
+            " = ", l.lr( dr_prime, r" - r \omega^2" ), hat_r, '+', l.frac('1', 'r'), prime( l.lr( r'r^2 \omega' ) ), hat_theta ) )
+        eq.shift( 2 * UP )
+
+        self.play( Write( eq ) )
+        self.wait( 5 )
+
+        eq2 = AcolorsMathTex( concat( vec_v, " = ", r" r \omega", hat_theta, r",\quad",
+            vec_a,
+            " = - r \omega^2", hat_r, "+ r", prime(r"\omega"), hat_theta ) )
+        eq2.move_to( eq )
+        self.play( TransformMatchingTex( eq, eq2 ) )
+        self.wait( 5 )
+
+        radius = 4
+        axes = Axes( x_range = [0, 1, 1], y_range = [0, 1, 1], x_length = radius, y_length = radius,
+                     axis_config = {"include_tip": True, "numbers_to_exclude": [0]} ).add_coordinates()
+
+        axes.shift( DOWN + 3 * LEFT )
+        origin = axes.coords_to_point( 0, 0 )
+        e1dir = RIGHT
+        e2dir = UP
+
+        rv = lambda t: e1dir * np.cos( t * 0.5 * PI ) + e2dir * np.sin( t * 0.5 * PI )
+        tv = lambda t: - e1dir * np.sin( t * 0.5 * PI ) + e2dir * np.cos( t * 0.5 * PI )
+
+        def omega(t):
+            #return 2 * PI * (t/4)^2
+            return 2 * PI * (t+1)/4
+            #return 1
+
+        def omegaprime(t):
+            #return 4 * PI * (t/4)
+            return PI/4
+            #return 0
+
+        t_parameter = ValueTracker(0)
+
+        scale = 0.25
+        # v = r \omega \thetacap
+        def velocity(t):
+            # logically, we are treating r = 1, but we have a scale factor for the rendering.
+            p = origin + radius * rv(t)
+            d = scale * omega(t) * tv(t)
+            return [p, d]
+
+        def uv(mob):
+            t = t_parameter.get_value()
+            x = velocity(t)
+            mob.put_start_and_end_on( x[0], x[0] + x[1] )
+
+        # a = -r \omega^2 \rcap + r \omega' \thetacap
+        def acceleration(t):
+            rt = rv(t)
+            tt = tv(t)
+            p = origin + radius * rt
+            o = omega(t)
+            d = scale * (- o * o * rt + omegaprime(t) * tt)
+            return [p, d]
+
+        def ua(mob):
+            t = t_parameter.get_value()
+            x = acceleration(t)
+            mob.put_start_and_end_on( x[0], x[0] + x[1] )
+
+        v1_i = velocity(0)
+        a1_i = acceleration(0)
+        v1 = Arrow( start = v1_i[0], end = v1_i[0] + v1_i[1], color = GREEN, buff = 0 ).add_updater(uv).update()
+        a1 = Arrow( start = a1_i[0], end = a1_i[0] + a1_i[1], color = RED, buff = 0 ).add_updater(ua).update()
+        #vtex = AcolorsMathTex( vec_v ).add_updater(uvt).update()
+        #atex = AcolorsMathTex( vec_a ).add_updater(uat).update()
+
+        g1 = ParametricFunction( lambda t: origin + radius * rv(t),
+                                 t_range=[0, 1],
+                                 scaling=axes.x_axis.scaling, color=YELLOW )
+        #self.add( VGroup( axes, g1, e1, e2, rtex, ttex, line ) )
+        self.play( AnimationGroup( Write( axes ), Write( g1 ), Write( v1 ), Write( a1 ) ) )
+        self.wait( 4 )
+
+        self.play( UpdateFromAlphaFunc( t_parameter,
+                                        lambda mob, alpha: mob.set_value( alpha ) ),
+                                        run_time=6 )
+        self.wait( 1 )
 
 # vim: et sw=4 ts=4
