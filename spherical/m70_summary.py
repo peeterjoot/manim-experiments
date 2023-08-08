b27d0bd76a7f67d941a666e51b742de0ff3ea7b4
Peeter Joot <peeter.joot@lzlabs.com>
2023-08-07 23:42:18 -0400

    modified:   m70_summary.py modified:   m80_velocity.py    -- delays

--- spherical/m70_summary.py
+++ spherical/m70_summary.py
@@ -7,7 +7,7 @@ class m70_summary( Scene ):
         title.move_to( 3 * UP )
         title.set_color( BLUE )
         self.play( FadeIn( title ) )
-        self.wait( 5 )
+        self.wait( 12 )
 
         eq = [ MathTex( concat( r'i = ', vec_e1, vec_e2, ',\quad j = ', vec_e3, vec_e1, r'e^{i\phi}' ) ),
                MathTex( concat( r'\hat{\mathbf{r}} =', vec_e3, r'e^{j\theta}' ) ),
@@ -19,31 +19,31 @@ class m70_summary( Scene ):
 
         eq[0].shift( 2 * UP + 0.5 * LEFT )
         self.play( Write( eq[0] ) )
-        self.wait( 5 )
+        self.wait( 12 )
 
         eq[1].move_to( eq[0] ).shift( 1.00 * DOWN + 3.0 * LEFT )
         self.play( Write( eq[1] ) )
-        self.wait( 5 )
+        self.wait( 12 )
     
         i = 1
         write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.37 * RIGHT, None )
-        self.wait( 5 )
+        self.wait( 12 )
 
         eq[3].move_to( eq[1] ).shift( 1.7 * DOWN + 0.57 * RIGHT )
         self.play( Write( eq[3] ) )
-        self.wait( 5 )
+        self.wait( 12 )
 
         i = 3
         write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.42 * RIGHT, None )
-        self.wait( 5 )
+        self.wait( 12 )
 
         eq[5].move_to( eq[3] ).shift( 1.7 * DOWN + 0.65 * LEFT )
         self.play( Write( eq[5] ) )
-        self.wait( 5 )
+        self.wait( 12 )
 
         i = 5
         write_aligned( self, eq[i], eq[i+1], 0.75 * DOWN + 0.48 * RIGHT, None )
-        self.wait( 5 )
+        self.wait( 12 )
 
         #for i in range(6):
         #    sh = 0.75 * DOWN + 0.0 * RIGHT
@@ -51,7 +51,7 @@ class m70_summary( Scene ):
         #        sh += 0.50 * RIGHT
         #    write_aligned( self, eq[i], eq[i+1], sh, None )
         #    self.wait( 3 )
-        #self.wait( 5 )
+        #self.wait( 12 )
 
         fadeall( self )
 
--- spherical/m80_velocity.py
+++ spherical/m80_velocity.py
@@ -7,7 +7,7 @@ class m80_velocity( Scene ):
         title.move_to( 3 * UP )
         title.set_color( BLUE )
         self.play( FadeIn( title ) )
-        self.wait( 5 )
+        self.wait( 10 )
 
         eq = [ MathTex( concat( r"\mathbf{x}' = r'", hat_r, r"+ r", rhat_prime ) ),
                MathTex( concat( rhat_prime, "=", vec_e3, l.frac( "d", "dt" ), l.lr( r"\cos\theta + j \sin\theta" ) ) ),
@@ -18,26 +18,25 @@ class m80_velocity( Scene ):
 
         eq[0].shift( 2 * UP + 0.5 * LEFT )
         self.play( Write( eq[0] ) )
-        self.wait( 5 )
+        self.wait( 10 )
 
         for i in range(5):
             sh = 1.00 * DOWN + 0.0 * RIGHT
             if i == 1:
                 sh += 0.50 * RIGHT
             write_aligned( self, eq[i], eq[i+1], sh, None )
-            self.wait( 3 )
-        self.wait( 10 )
+            self.wait( 10 )
 
         eq2 = [ MathTex( concat( r"\mathbf{x}' = r'", hat_r, r"+ r \hat{\boldsymbol{\theta}} \theta' + r \sin\theta \hat{\boldsymbol{\phi}} \phi' " ) ),
                 MathTex( concat( l.frac( "1", "2" ), "m {\mathbf{v}}^2 = ", l.frac( "m", "2" ), l.lr( r"{r'}^2 + r^2 {\theta'}^2 + r^2 \sin^2 \theta {\phi'}^2", big = 1 ) ) ) ]
 
         eq2[0].shift( 1 * UP )
         self.play( ReplacementTransform( VGroup(*eq), eq2[0] ) )
-        self.wait( 5 )
+        self.wait( 10 )
 
         eq2[1].move_to( eq2[0] ).shift( 2 * DOWN )
         self.play( Write( eq2[1] ) )
-        self.wait( 5 )
+        self.wait( 10 )
 
         fadeall( self )
 
