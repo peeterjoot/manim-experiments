f2c61df468eda04b6abbc5e37237caf4901f18e6
Peeter Joot <peeter.joot@lzlabs.com>
2023-09-05 22:23:31 -0400

    m010_Intro.py    Intro page, before animation.

--- sphericalUnit/m010_Intro.py
+++ sphericalUnit/m010_Intro.py
@@ -3,10 +3,25 @@ from helper import *
 class m010_Intro( Scene ):
     def construct( self ):
 
-        title = Text( 'Unit vector derivative.' )
+        title = Text( 'Radial representations.' )
         title.move_to( 3 * UP )
         title.set_color( BLUE )
         self.play( FadeIn( title ) )
+        self.wait( 5 )
+
+        eq = MathTex( concat( vec_x, " = r ", hat_r ) )
+        eq.shift( UP )
+        self.play( Write( eq ) )
+        self.wait( 5 )
+
+        eq2 = MathTex( concat( vec_x, " = r' ", hat_r, " + r ", rhat_prime ) )
+        eq2.move_to( eq ).shift( DOWN * 1.5 )
+        self.play( Write( eq2 ) )
+        self.wait( 5 )
+
+        eq3 = MathTex( concat( rhat_prime, " = ?") )
+        eq3.move_to( eq2 ).shift( DOWN * 1.5 )
+        self.play( Write( eq3 ) )
         self.wait( 5 )
 
         fadeall( self )
