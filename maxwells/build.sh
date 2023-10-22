51bca1868b6de34ff7205ffac10b10ee8d0729dc
Peeter Joot <peeter.joot@lzlabs.com>
2023-10-22 13:20:57 -0400

    .

--- maxwells/build.sh
+++ maxwells/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 production=0
-#production=1
+production=1
 
 if [ $production -eq 1 ] ; then
 q='-q h'
@@ -10,10 +10,10 @@ p='-p'
 fi
 
 #./clean.sh
-#../bin/build $q $p m010_Intro.py
-#../bin/build $q $p m020_isotropic.py
-#../bin/build $q $p m030_divcurl.py
-#../bin/build $q $p m040_maxwells.py
-#../bin/build $q $p m050_recover.py
+../bin/build $q $p m010_Intro.py
+../bin/build $q $p m020_isotropic.py
+../bin/build $q $p m030_divcurl.py
+../bin/build $q $p m040_maxwells.py
+../bin/build $q $p m050_recover.py
 ../bin/build $q $p m100_summary.py
 #echo done
