d6f8d1cdd44e4e8546211bad0fcc8eb90e92cab2
Peeter Joot <peeter.joot@lzlabs.com>
2023-10-24 20:41:36 -0400

    summary.tweak

--- maxwells/m100_summary.py
+++ maxwells/m100_summary.py
@@ -7,7 +7,7 @@ class m100_summary( Scene ):
         title.move_to( 3 * UP )
         title.set_color( BLUE )
         self.play( FadeIn( title ) )
-        self.wait( 5 )
+        self.wait( 0.25 )
 
         eq = [ cMathTex( r"{{ \boldsymbol{\nabla} }} \times {{ \mathbf{E} }} = -{{ \mathbf{M} }} - { \partial {{ \mathbf{B} }} \over \partial t } " ),
                cMathTex( r"{{ \boldsymbol{\nabla} }} \times {{ \mathbf{H} }} = {{ \mathbf{J} }} + { \partial {{ \mathbf{D} }} \over \partial t } " ),
@@ -17,7 +17,7 @@ class m100_summary( Scene ):
                cMathTex( r"{{ \mathbf{D} }} = \epsilon {{ \mathbf{E} }},\, {{ \mathbf{B} }} = \mu {{ \mathbf{H} }}" ) ]
         eq[0].shift( 2.00 * UP + 3.00 * LEFT )
         self.play( Write( eq[0] ) )
-        self.wait( 5 )
+        self.wait( 1 )
 
         for i in range(5):
             sh = 1.00 * DOWN
@@ -29,11 +29,29 @@ class m100_summary( Scene ):
                 sh += 0.00 * DOWN + 1.40 * LEFT
             if i == 3:
                 sh += 0.00 * DOWN + 2.00 * LEFT
+                self.wait( 1 )
+            if i == 4:
+                self.wait( 7 )
             write_aligned( self, eq[i], eq[i+1], sh, None )
-            self.wait( 5 )
+            self.wait( 0.25 )
         #self.play( FadeOut( VGroup( *eq ) ) )
-        self.wait( 5 )
+        self.wait( 3 )
 
+        eqEH = [ cMathTex( r"{{ \boldsymbol{\nabla} }} \times {{ \mathbf{E} }} = -{{ \mathbf{M} }} - \mu { \partial {{ \mathbf{H} }} \over \partial t } " ),
+                 cMathTex( r"{{ \boldsymbol{\nabla} }} \times {{ \mathbf{H} }} = {{ \mathbf{J} }} + \epsilon { \partial {{ \mathbf{E} }} \over \partial t } " ),
+                 cMathTex( r"{{ \boldsymbol{\nabla} }} \cdot {{ \mathbf{E} }} = \rho/\epsilon" ),
+                 cMathTex( r"{{ \boldsymbol{\nabla} }} \cdot {{ \mathbf{H} }} = \rho_{\mathrm{m}}/\mu" ) ]
+
+        for i in range(4):
+            eqEH[i].move_to(eq[i])
+
+        self.play( AnimationGroup(
+                ReplacementTransform( eq[0], eqEH[0] ),
+                ReplacementTransform( eq[1], eqEH[1] ),
+                ReplacementTransform( eq[2], eqEH[2] ),
+                ReplacementTransform( eq[3], eqEH[3] ) ) )
+        self.wait( 7 )
+ 
         eq2 = [ cMathTex( r"\Bigl( \boldsymbol{\nabla} + { 1 \over c } { \partial \over \partial t } \Bigr) {{ F }} = J" ),
                 cMathTex( r"{{ F }} = {{ \mathbf{E} }} + I \eta {{ \mathbf{H} }}" ),
                 cMathTex( r"J = \eta( c \rho - {{ \mathbf{J} }} ) + I ( c \rho_{\mathrm{m}} - {{ \mathbf{M} }} )" ) ]
