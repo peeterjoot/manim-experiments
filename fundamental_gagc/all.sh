dd6c6bfe61cb2b0fb461a10e9dbc56b697ce0eb4
Peeter Joot <peeter.joot@lzlabs.com>
2023-11-02 23:59:49 -0400

    modified:   all modified:   all.sh modified:   build.sh modified:   m050_vectorderivative.py

--- fundamental_gagc/all.sh
+++ fundamental_gagc/all.sh
@@ -1,2 +1,2 @@
 find media/videos/*/1080p60 -name "*.mp4" | grep -v partial
-find media/videos/*/480p15 -name "*.mp4" | grep -v partial
+#find media/videos/*/480p15 -name "*.mp4" | grep -v partial
--- fundamental_gagc/m050_vectorderivative.py
+++ fundamental_gagc/m050_vectorderivative.py
@@ -12,7 +12,7 @@ class m050_vectorderivative( Scene ):
         eq = [ cMathTex( r"{{ \mathbf{x} }} = {{ \mathbf{x} }} (a_1, a_2, \cdots, a_k)" ),
                cMathTex( r"{{ \mathbf{x} }}_{i} = {\partial {{ \mathbf{x} }} \over \partial a_i}" ),
                cMathTex( r"I^k = {{ \mathbf{x} }}{}_{1} \wedge {{ \mathbf{x} }}{}_{2} \cdots \wedge {{ \mathbf{x} }}{}_{k},\quad d^k {{ \mathbf{x} }} = I^k \, da_1 da_2 \cdots da_k" ),
-               cMathTex( r"{{ \mathbf{x} }}_{i} \cdot {{ \mathbf{x} }}^{j} = {\delta_i}^j" ),
+               cMathTex( r"{{ \mathbf{x} }}^{i} \cdot {{ \mathbf{x} }}_{j} = {\delta^i}_j, \textrm{where}\, {{ \mathbf{x} }}^{i} \in \textrm{span}\{ {{ \mathbf{x} }}_{1}, {{ \mathbf{x} }}_{2}, \cdots \}" ),
                cMathTex( r"\boldsymbol{\partial} = \sum_{i = 0}^k {{ \mathbf{x} }}^{i} {\partial \over \partial a_i} = \boldsymbol{\nabla} \cdot I^k {1 \over I^k}" ) ]
 
         eq[0].shift( 1.50 * UP + 0 * RIGHT )
@@ -26,9 +26,9 @@ class m050_vectorderivative( Scene ):
             if i == 1:
                 sh += 0.00 * DOWN + 3.00 * LEFT
             if i == 2:
-                sh += 0.00 * DOWN + 1.80 * RIGHT
+                sh += 0.00 * DOWN + 1.00 * RIGHT
             if i == 3:
-                sh += 0.30 * DOWN + 0.10 * LEFT
+                sh += 0.30 * DOWN + 0.00 * LEFT
             write_aligned( self, eq[i], eq[i+1], sh, None )
             self.wait( 1 )
 
