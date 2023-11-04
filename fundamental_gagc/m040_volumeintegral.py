e3edacf42594059414f9fea04e9bd6202043781e
Peeter Joot <peeter.joot@lzlabs.com>
2023-11-04 13:38:38 -0400

    modified:   all modified:   build.sh modified:   m003_theorem.py deleted:    m010_Intro.py modified:   m040_volumeintegral.py new file:   m050_reciprocalbasis.py

--- fundamental_gagc/m040_volumeintegral.py
+++ fundamental_gagc/m040_volumeintegral.py
@@ -29,9 +29,45 @@ class m040_volumeintegral( Scene ):
             write_aligned( self, eq[i], eq[i+1], sh, None )
             self.wait( 1 )
 
-        g = VGroup( *eq )
-        self.play( g.animate.shift(4.00 * LEFT), run_time=1, rate_func=linear )
-        self.wait( 8 )
+        #g = VGroup( *eq )
+        #self.play( g.animate.shift(4.00 * LEFT), run_time=1, rate_func=linear )
+        #self.wait( 8 )
+
+        title2 = Text( "Volume integrals: example." )
+        title2.move_to( title ).shift( 0 * LEFT )
+        title2.set_color( BLUE )
+        self.play( AnimationGroup( FadeOut( *eq ), ReplacementTransform( title, title2 ) ) )
+
+        eq2 = [ cMathTex( r"{{ \mathbf{x} }}(r, \phi, z) = r \mathbf{e}_1 e^{i\phi} + z \mathbf{e}_3, \quad i = \mathbf{e}_{12}" ),
+                cMathTex( r"{{ \mathbf{x} }}_1 = \mathbf{e}_1 e^{i\phi}, \quad {{ \mathbf{x} }}_1 = r \mathbf{e}_2 e^{i\phi}, \quad {{ \mathbf{x} }}_3 = \mathbf{e}_3" ),
+                cMathTex( r"d^3 {{ \mathbf{x} }} = r^2 \mathbf{e}_{123}\, dr d\phi dz,\quad F = \mathbf{e}_1, \quad G = z \mathbf{e}_4" ),
+                cMathTex( r"\int_{r=0}^{R} \int_{\phi=0}^{\phi} \int_{z=0}^{Z} F d^3 x G =" ),
+                cMathTex( r"\int_{r=0}^{R} \int_{\phi=0}^{\phi} \int_{z=0}^{Z} r^2 \mathbf{e}_{234} dr d\phi z dz" ),
+                cMathTex( r"{1 \over 6} \mathbf{e}_{234} R^3 \pi Z^2" ) ]
+
+        eq2[0].shift( 1.50 * UP + 0 * RIGHT )
+        self.play( Write( eq2[0] ) )
+        self.wait( 1 )
+        for i in range(3):
+            sh = 1.00 * DOWN + 0.5 * LEFT
+            if i == 0:
+                sh += 0.00 * DOWN + 0.60 * RIGHT
+            if i == 1:
+                sh += 0.00 * DOWN + 1.00 * LEFT
+            if i == 2:
+                sh += 0.20 * DOWN + 2.00 * LEFT
+            write_aligned( self, eq2[i], eq2[i+1], sh, None )
+            self.wait( 1 )
+        eq2[4].move_to( eq2[3], RIGHT ).shift( 6.00 * RIGHT )
+        self.play( Write( eq2[4] ) )
+        self.wait( 1 )
+        i=5
+        sh = 0.00 * RIGHT
+        eq2[i].move_to( eq2[4], LEFT ).shift( sh )
+        self.play( ReplacementTransform( eq2[i-1], eq2[i] ) )
+        #self.wait( 1 )
+        #self.play( FadeOut( VGroup( eq2[0], eq2[1], eq2[2], eq2[3], eq2[5] ) ) )
+        self.wait( 5 )
 
         fadeall( self )
 
---
+++ fundamental_gagc/m050_reciprocalbasis.py
@@ -0,0 +1,113 @@
+from mycolors import *
+
+class m050_reciprocalbasis( Scene ):
+    def construct( self ):
+
+        title = Text( "Reciprocal basis." )
+        title.move_to( 3 * UP )
+        title.set_color( BLUE )
+        self.play( FadeIn( title ) )
+        self.wait( 1 )
+
+        eq = [ cMathTex( r"{{ \mathbf{x} }}^{i} \cdot {{ \mathbf{x} }}_{j} = {\delta^i}_j, \textrm{where}\, {{ \mathbf{x} }}^{i} \in \textrm{span}\{ {{ \mathbf{x} }}_{1}, {{ \mathbf{x} }}_{2}, \cdots \}" ),
+               cMathTex( r"\textrm{Theorem: Given}\, {{ \mathbf{x} }}_1, {{ \mathbf{x} }}_2" ),
+               cMathTex( r"{{ \mathbf{x} }} {}^1 = {{ \mathbf{x} }}_2 \cdot \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1}" ),
+               cMathTex( r"{{ \mathbf{x} }} {}^2 = -{{ \mathbf{x} }}_1 \cdot \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1}." ) ]
+
+        eq[0].shift( 1.50 * UP + 0 * RIGHT )
+        self.play( Write( eq[0] ) )
+        self.wait( 1 )
+        eq[1].move_to( eq[0] ).shift( 1.00 * DOWN + 0 * RIGHT )
+        self.play( Write( eq[1] ) )
+        self.wait( 1 )
+        eq[2].move_to( eq[1] ).shift( 1.00 * DOWN + 0 * RIGHT )
+        self.play( Write( eq[2] ) )
+        self.wait( 1 )
+        eq[3].move_to( eq[2] ).shift( 1.00 * DOWN + 0 * RIGHT )
+        self.play( Write( eq[3] ) )
+        self.wait( 1 )
+
+        g = VGroup( eq[1], eq[2], eq[3] )
+        self.play( g.animate.shift(4.00 * LEFT), run_time=1, rate_func=linear )
+        self.wait( 8 )
+
+        eq2 = [ cMathTex( r"{{ \mathbf{x} }}_1 \cdot {{ \mathbf{x} }}^1 = \bigl\langle {{ \mathbf{x} }}_1 \Bigl( {{ \mathbf{x} }}_2 \cdot \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \Bigr) \bigr\rangle" ),
+                cMathTex( r"= \bigl\langle {{ \mathbf{x} }}_1 {{ \mathbf{x} }}_2 \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= \bigl\langle ({{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2) \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= 1" ) ]
+        eq2[0].move_to( eq[2] ).shift( 0.00 * UP + 6.00 * RIGHT )
+        self.play( Write( eq2[0] ) )
+        self.wait( 1 )
+        for i in range(3):
+            sh = 0.75 * DOWN
+            if i == 0:
+                sh += 0.00 * DOWN + 0.34 * RIGHT
+            #if i == 2:
+            #    sh += 0.00 * DOWN + 1.42 * LEFT
+            write_aligned( self, eq2[i], eq2[i+1], sh, None )
+            self.wait( 1 )
+        self.wait( 3 )
+        self.play( FadeOut( *eq2 ) )
+        self.wait( 1 )
+
+        eq3 = [ cMathTex( r"{{ \mathbf{x} }}_2 \cdot {{ \mathbf{x} }}^1 = \bigl\langle {{ \mathbf{x} }}_2 \Bigl( {{ \mathbf{x} }}_2 \cdot \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \Bigr) \bigr\rangle" ),
+                cMathTex( r"= \bigl\langle {{ \mathbf{x} }}_2 {{ \mathbf{x} }}_2 \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= \bigl\langle \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= 0" ) ]
+        eq3[0].move_to( eq[2] ).shift( 0.00 * UP + 6.00 * RIGHT )
+        self.play( Write( eq3[0] ) )
+        self.wait( 1 )
+        for i in range(3):
+            sh = 0.75 * DOWN
+            if i == 0:
+                sh += 0.00 * DOWN + 0.34 * RIGHT
+            #if i == 2:
+            #    sh += 0.00 * DOWN + 1.42 * LEFT
+            write_aligned( self, eq3[i], eq3[i+1], sh, None )
+            self.wait( 1 )
+        self.wait( 3 )
+        self.play( FadeOut( *eq3 ) )
+        self.wait( 1 )
+
+        eq4 = [ cMathTex( r"{{ \mathbf{x} }}_1 \cdot {{ \mathbf{x} }}^2 = - \bigl\langle {{ \mathbf{x} }}_1 \Bigl( {{ \mathbf{x} }}_1 \cdot \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \Bigr) \bigr\rangle" ),
+                cMathTex( r"= - \bigl\langle {{ \mathbf{x} }}_1 {{ \mathbf{x} }}_1 \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= -\bigl\langle \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= 0" ) ]
+        eq4[0].move_to( eq[2] ).shift( 0.00 * UP + 6.00 * RIGHT )
+        self.play( Write( eq4[0] ) )
+        self.wait( 1 )
+        for i in range(3):
+            sh = 0.75 * DOWN
+            if i == 0:
+                sh += 0.00 * DOWN + 0.34 * RIGHT
+            #if i == 2:
+            #    sh += 0.00 * DOWN + 1.42 * LEFT
+            write_aligned( self, eq4[i], eq4[i+1], sh, None )
+            self.wait( 1 )
+        self.wait( 3 )
+        self.play( FadeOut( *eq4 ) )
+        self.wait( 1 )
+
+        eq5 = [ cMathTex( r"{{ \mathbf{x} }}_2 \cdot {{ \mathbf{x} }}^2 = - \bigl\langle {{ \mathbf{x} }}_2 \Bigl( {{ \mathbf{x} }}_1 \cdot \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \Bigr) \bigr\rangle" ),
+                cMathTex( r"= - \bigl\langle {{ \mathbf{x} }}_2 {{ \mathbf{x} }}_1 \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= \bigl\langle ({{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2) \bigl( {{ \mathbf{x} }}_1 \wedge {{ \mathbf{x} }}_2 } \bigr) {}^{-1} \bigr\rangle" ),
+                cMathTex( r"= 1." ) ]
+        eq5[0].move_to( eq[2] ).shift( 0.00 * UP + 6.00 * RIGHT )
+        self.play( Write( eq5[0] ) )
+        self.wait( 1 )
+        for i in range(3):
+            sh = 0.75 * DOWN
+            if i == 0:
+                sh += 0.00 * DOWN + 0.34 * RIGHT
+            #if i == 3:
+            #    sh += 0.00 * DOWN + 1.42 * LEFT
+            write_aligned( self, eq5[i], eq5[i+1], sh, None )
+            self.wait( 1 )
+        self.wait( 3 )
+        self.play( FadeOut( *eq5 ) )
+        self.wait( 1 )
+
+        self.wait( 5 )
+        fadeall( self )
+
+# vim: et sw=4 ts=4
