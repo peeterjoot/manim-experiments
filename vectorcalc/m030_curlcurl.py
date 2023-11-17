426a3d3d1fd3dd32f2a9c0635b9964bbfa0a2f94
Peeter Joot <peeter.joot@lzlabs.com>
2023-11-16 22:53:06 -0500

    modified:   m030_curlcurl.py

--- vectorcalc/m030_curlcurl.py
+++ vectorcalc/m030_curlcurl.py
@@ -9,96 +9,14 @@ class m030_curlcurl( Scene ):
         self.play( FadeIn( title ) )
         self.wait( 1 )
 
-        eq = [ [ cMathTex( r"\boldsymbol{\nabla} \times ( \boldsymbol{\nabla} f )"), cMathTex( " = "), cMathTex(" 0 " ) ],
-               [ cMathTex( r"\boldsymbol{\nabla} \cdot ( \boldsymbol{\nabla} \times {{ \mathbf{f} }} )"), cMathTex( "=" ), cMathTex( r"0" ) ],
-               [ cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge A )"), cMathTex( "=" ), cMathTex( r"0" ) ] ]
-
-        ref = title
-        for i in range(3):
-            eq[i][0].next_to( ref, DOWN )
-            eq[i][1].next_to( eq[i][0], RIGHT )
-            eq[i][2].next_to( eq[i][1], RIGHT )
-            for j in range(3):
-                self.play( Write( eq[i][j] ) )
-            ref = eq[i][0]
-            self.wait( 1 )
-
-        g1 = VGroup( *eq[0] )
-        g2 = VGroup( *eq[1] )
-        g3 = VGroup( *eq[2] )
-        self.play( AnimationGroup( g1.animate.shift(5.0 * LEFT + 0 * DOWN),
-                                   g2.animate.shift(5.0 * LEFT + 0 * DOWN),
-                                   g3.animate.shift(5.0 * LEFT + 0 * DOWN) ), run_time=3, rate_func=linear )
-
-        eq2m = [ cMathTex( r"f \in \mathbb{R}" ),
-                 cMathTex( r"0" ),
-                 cMathTex( r"=" ), cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge f )" ),
-                 cMathTex( r"=" ), cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} f )" ),
-                 cMathTex( r"=" ), cMathTex( r"I (\boldsymbol{\nabla} \times ( \boldsymbol{\nabla} f ))" ) ]
-
-        eq2m[0].next_to( title, DOWN )
-        eq2m[1].next_to( eq2m[0], DOWN )
-        eq2m[2].next_to( eq2m[1], RIGHT )
-        eq2m[3].next_to( eq2m[2], RIGHT )
-        self.play( Write( eq2m[0] ) )
-        self.wait( 1 )
-        self.play( Write( eq2m[1] ) )
-        self.play( Write( eq2m[2] ) )
-        self.play( Write( eq2m[3] ) )
-        self.wait( 1 )
-        ref = eq2m[2]
-        j = 4
-        for i in range(2):
-            eq2m[j].next_to( ref, DOWN ).shift( 0.3 * DOWN )
-            eq2m[j+1].next_to( eq2m[j], RIGHT )
-            for k in range(2):
-                self.play( Write( eq2m[j+k] ) )
-            ref = eq2m[j]
-            self.wait( 1 )
-            j += 2
-        self.play( Indicate( g1 ) )
-        self.wait( 5 )
-        self.play( FadeOut( VGroup( *eq2m ) ) )
-        self.wait( 1 )
-
-        eq3m = [ cMathTex( r"{{ \mathbf{f} }} \in \mathbb{R}^N" ),
-                 cMathTex( r"0" ),
-                 cMathTex( r"=" ), cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge {{ \mathbf{f} }})" ),
-                 cMathTex( r"=" ), cMathTex( r"I (\boldsymbol{\nabla} \cdot ( \boldsymbol{\nabla} \times {{ \mathbf{f} }}))" ) ]
-
-        eq3m[0].next_to( title, DOWN )
-        eq3m[1].next_to( eq3m[0], DOWN )
-        eq3m[2].next_to( eq3m[1], RIGHT )
-        eq3m[3].next_to( eq3m[2], RIGHT )
-        self.play( Write( eq3m[0] ) )
-        self.wait( 1 )
-        self.play( Write( eq3m[1] ) )
-        self.play( Write( eq3m[2] ) )
-        self.play( Write( eq3m[3] ) )
-        self.wait( 1 )
-        ref = eq3m[2]
-        j = 4
-        for i in range(1):
-            eq3m[j].next_to( ref, DOWN ).shift( 0.3 * DOWN )
-            eq3m[j+1].next_to( eq3m[j], RIGHT )
-            for k in range(2):
-                self.play( Write( eq3m[j+k] ) )
-            ref = eq3m[j]
-            self.wait( 1 )
-            j += 2
-        self.play( Indicate( g2 ) )
-        self.wait( 5 )
-        self.play( FadeOut( VGroup( *eq3m ) ) )
-        self.wait( 1 )
-
         eq2 = [ cMathTex( r"f \in \mathbb{R}" ),
                 cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge f )" ),
                 cMathTex( r"=" ), cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} f )" ),
                 cMathTex( r"=" ), cMathTex( r"\sum_{ij} ({{ \mathbf{e} }}_{i} \wedge {{ \mathbf{e} }}_{j}) { \partial^2 f \over { \partial x_i \partial x_j} }" ),
                 cMathTex( r"=" ), cMathTex( r"0" ) ]
 
-        eq2[0].next_to( title, DOWN )
-        eq2[1].next_to( eq2[0], DOWN )
+        eq2[0].next_to( title, DOWN ).shift( 2.0 * LEFT + 2.0 * RIGHT )
+        eq2[1].next_to( eq2[0], DOWN ).shift( 1.0 * DOWN - 2.0 * RIGHT )
         eq2[2].next_to( eq2[1], RIGHT )
         eq2[3].next_to( eq2[2], RIGHT )
         self.play( Write( eq2[0] ) )
@@ -117,18 +35,17 @@ class m030_curlcurl( Scene ):
             ref = eq2[j]
             self.wait( 1 )
             j += 2
-        self.play( Indicate( g3 ) )
-        self.wait( 5 )
+        #self.play( Indicate( g3 ) )
         self.play( FadeOut( VGroup( *eq2 ) ) )
-        self.wait( 1 )
+        self.wait( 5 )
 
         eq3 = [ cMathTex( r"{{ \mathbf{f} }} \in \mathbb{R}^N" ),
                 cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge {{ \mathbf{f} }} )" ),
                 cMathTex( r"=" ), cMathTex( r"\sum_{ij} ({{ \mathbf{e} }}_{i} \wedge {{ \mathbf{e} }}_{j} \wedge {{ \mathbf{e} }}_{k}) { \partial^2 f_k \over { \partial x_i \partial x_j} }" ),
                 cMathTex( r"=" ), cMathTex( r"0" ) ]
 
-        eq3[0].next_to( title, DOWN )
-        eq3[1].next_to( eq3[0], DOWN )
+        eq3[0].next_to( title, DOWN ).shift( 0.0 * LEFT )
+        eq3[1].next_to( eq3[0], DOWN ).shift( 1.0 * DOWN - 2.0 * RIGHT )
         eq3[2].next_to( eq3[1], RIGHT )
         eq3[3].next_to( eq3[2], RIGHT )
         self.play( Write( eq3[0] ) )
@@ -147,11 +64,111 @@ class m030_curlcurl( Scene ):
             ref = eq3[j]
             self.wait( 1 )
             j += 2
-        self.play( Indicate( g3 ) )
-        self.wait( 5 )
+        #self.play( Indicate( g3 ) )
         self.play( FadeOut( VGroup( *eq3 ) ) )
+        self.wait( 5 )
+
+        eq = [ [ cMathTex( r"\boldsymbol{\nabla} \times ( \boldsymbol{\nabla} f )"), cMathTex( " = "), cMathTex(" 0 " ) ],
+               [ cMathTex( r"\boldsymbol{\nabla} \cdot ( \boldsymbol{\nabla} \times {{ \mathbf{f} }} )"), cMathTex( "=" ), cMathTex( r"0" ) ],
+               [ cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge A )"), cMathTex( "=" ), cMathTex( r"0" ) ] ]
+
+        ref = title
+        i = 2
+        eq[i][0].next_to( ref, DOWN ).shift( 4.0 * DOWN + 5.0 * LEFT )
+        eq[i][1].next_to( eq[i][0], RIGHT )
+        eq[i][2].next_to( eq[i][1], RIGHT )
+        for j in range(3):
+            self.play( Write( eq[i][j] ) )
+        ref = eq[i][0]
         self.wait( 1 )
 
+        #g1 = VGroup( *eq[0] )
+        #g2 = VGroup( *eq[1] )
+        #g3 = VGroup( *eq[2] )
+        #self.play( AnimationGroup( g1.animate.shift(5.0 * LEFT + 0 * DOWN),
+        #                           g2.animate.shift(5.0 * LEFT + 0 * DOWN),
+        #                           g3.animate.shift(5.0 * LEFT + 0 * DOWN) ), run_time=3, rate_func=linear )
+
+        eq2m = [ cMathTex( r"f \in \mathbb{R}" ),
+                 cMathTex( r"0" ),
+                 cMathTex( r"=" ), cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge f )" ),
+                 cMathTex( r"=" ), cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} f )" ),
+                 cMathTex( r"=" ), cMathTex( r"I (\boldsymbol{\nabla} \times ( \boldsymbol{\nabla} f ))" ) ]
+
+        eq2m[0].next_to( title, DOWN ).shift( 0.0 * LEFT )
+        eq2m[1].next_to( eq2m[0], DOWN ).shift( 1.0 * DOWN - 2.0 * RIGHT )
+        eq2m[2].next_to( eq2m[1], RIGHT )
+        eq2m[3].next_to( eq2m[2], RIGHT )
+        self.play( Write( eq2m[0] ) )
+        self.wait( 1 )
+        self.play( Write( eq2m[1] ) )
+        self.play( Write( eq2m[2] ) )
+        self.play( Write( eq2m[3] ) )
+        self.wait( 1 )
+        ref = eq2m[2]
+        j = 4
+        for i in range(2):
+            eq2m[j].next_to( ref, DOWN ).shift( 0.3 * DOWN )
+            eq2m[j+1].next_to( eq2m[j], RIGHT )
+            for k in range(2):
+                self.play( Write( eq2m[j+k] ) )
+            ref = eq2m[j]
+            self.wait( 1 )
+            j += 2
+        #self.play( Indicate( g1 ) )
+        self.play( FadeOut( VGroup( *eq2m ) ) )
+        self.wait( 5 )
+
+        ref = eq[2][0]
+        i = 0
+        eq[i][0].next_to( ref, DOWN )
+        eq[i][1].next_to( eq[i][0], RIGHT )
+        eq[i][2].next_to( eq[i][1], RIGHT )
+        for j in range(3):
+            self.play( Write( eq[i][j] ) )
+        self.wait( 5 )
+
+        eq3m = [ cMathTex( r"{{ \mathbf{f} }} \in \mathbb{R}^N" ),
+                 cMathTex( r"0" ),
+                 cMathTex( r"=" ), cMathTex( r"\boldsymbol{\nabla} \wedge ( \boldsymbol{\nabla} \wedge {{ \mathbf{f} }})" ),
+                 cMathTex( r"=" ), cMathTex( r"I (\boldsymbol{\nabla} \cdot ( \boldsymbol{\nabla} \times {{ \mathbf{f} }}))" ) ]
+
+        eq3m[0].next_to( title, DOWN ).shift( 0.0 * LEFT )
+        eq3m[1].next_to( eq3m[0], DOWN ).shift( 1.0 * DOWN - 2.0 * RIGHT )
+        eq3m[2].next_to( eq3m[1], RIGHT )
+        eq3m[3].next_to( eq3m[2], RIGHT )
+        self.play( Write( eq3m[0] ) )
+        self.wait( 1 )
+        self.play( Write( eq3m[1] ) )
+        self.play( Write( eq3m[2] ) )
+        self.play( Write( eq3m[3] ) )
+        self.wait( 1 )
+        ref = eq3m[2]
+        j = 4
+        for i in range(1):
+            eq3m[j].next_to( ref, DOWN ).shift( 0.3 * DOWN )
+            eq3m[j+1].next_to( eq3m[j], RIGHT )
+            for k in range(2):
+                self.play( Write( eq3m[j+k] ) )
+            ref = eq3m[j]
+            self.wait( 1 )
+            j += 2
+        #self.play( Indicate( g2 ) )
+        self.play( FadeOut( VGroup( *eq3m ) ) )
+        self.wait( 5 )
+
+        ref = eq[0][0]
+        i = 1
+        eq[i][0].next_to( ref, DOWN )
+        eq[i][1].next_to( eq[i][0], RIGHT )
+        eq[i][2].next_to( eq[i][1], RIGHT )
+        for j in range(3):
+            self.play( Write( eq[i][j] ) )
+
+        g = VGroup( *eq[0] )
+        g += VGroup( *eq[1] )
+        g += VGroup( *eq[2] )
+        self.play( g.animate.shift( 4.5 * RIGHT + 2.0 * UP ), run_time=3, rate_func=linear )
 
         self.wait( 5 )
         fadeall( self )
