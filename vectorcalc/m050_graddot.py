239f336959d5f67407051f0108fb956bc07ad503
Peeter Joot <peeter.joot@lzlabs.com>
2023-11-19 09:31:08 -0500

    m050_graddot.py -- switched to new aligned helper function.  Takes arrays of MathTex's modelling the write of:
    \begin{aligned}
    a &= b0 + b1 \\
      &= c0 + c1 + c2
      &= d0 + d1 + d2 + d3
    \end{aligned}
    
    The input would look like:
    
    eq = [ [ MathTex( r"a" )
             MathTex( r"=" )
             MathTex( r"b0" ), MathTex( r"+ b1" ) ],
           [ MathTex( r"=" )
             MathTex( r"c0" ), MathTex( r"+ c1" ), MathTex( r"+ c2" ) ],
           [ MathTex( r"=" )
             MathTex( r"d0" ), MathTex( r"+ d1" ), MathTex( r"+ d2" ), MathTex( r"+ d3" ) ] ]

--- vectorcalc/m050_graddot.py
+++ vectorcalc/m050_graddot.py
@@ -34,12 +34,8 @@ class m050_graddot( Scene ):
                  cMathTex( r"=" ),
                  cMathTex( r"({{ \mathbf{a} }} \cdot \boldsymbol{\nabla}) {{ \mathbf{b} }} + ({{ \mathbf{b} }} \cdot \boldsymbol{\nabla}) {{ \mathbf{a} }} + {{ \mathbf{b} }} \times (\boldsymbol{\nabla} \times {{ \mathbf{a} }}) + {{ \mathbf{a} }} \times (\boldsymbol{\nabla} \times {{ \mathbf{b} }})" ) ] ]
 
-        eq[0][1].next_to( title, DOWN ).shift( 4.0 * LEFT + 0.2 * DOWN )
-        eq[0][0].next_to( eq[0][1], LEFT )
-        eq[0][2].next_to( eq[0][1], RIGHT )
-        for i in range(3):
-            self.play( Write( eq[0][i] ) )
-        self.wait( 1 )
+        aligned( self, eq, title, 4.0 * LEFT + 0.2 * DOWN, 0.3 * DOWN, 1, 1 )
+
         self.play( FadeOut( *eq[0] ) )
         self.wait( 1 )
 
@@ -48,17 +44,8 @@ class m050_graddot( Scene ):
                   cMathTex( r"\langle \boldsymbol{\nabla} ({{ \mathbf{a} }} {{ \mathbf{b} }} - {{ \mathbf{a} }} \wedge {{ \mathbf{b} }}) \rangle {}_1" ) ],
                 [ cMathTex( r"=" ),
                   cMathTex( r"\langle \boldsymbol{\nabla} {{ \mathbf{a} }} {{ \mathbf{b} }} \rangle {}_1 - \boldsymbol{\nabla} \cdot ({{ \mathbf{a} }} \wedge {{ \mathbf{b} }})" ) ] ]
-        eq2[0][1].next_to( title, DOWN ).shift( 4.0 * LEFT + 0.2 * DOWN )
-        eq2[0][0].next_to( eq2[0][1], LEFT )
-        eq2[0][2].next_to( eq2[0][1], RIGHT )
-        for i in range(3):
-            self.play( Write( eq2[0][i] ) )
-        self.wait( 1 )
-        eq2[1][0].next_to( eq2[0][1], DOWN ).shift( 0.3 * DOWN )
-        eq2[1][1].next_to( eq2[1][0], RIGHT )
-        for i in range(2):
-            self.play( Write( eq2[1][i] ) )
-        self.wait( 1 )
+
+        aligned( self, eq2, title, 4.0 * LEFT + 0.2 * DOWN, 0.3 * DOWN, 1, 1 )
 
         eq3 = [ [ cMathTex( r"\langle \boldsymbol{\nabla} {{ \mathbf{a} }} {{ \mathbf{b} }} \rangle {}_1" ),
                   cMathTex( r"=" ),
@@ -68,25 +55,14 @@ class m050_graddot( Scene ):
                   cMathTex( r"\langle (\boldsymbol{\nabla} \cdot {{ \mathbf{a} }} + \boldsymbol{\nabla} \wedge {{ \mathbf{a} }}) {{ \mathbf{b} }}"
                             r"- {{ \mathbf{a} }} \boldsymbol{\nabla}' {{ \mathbf{b} }}' + 2 ({{ \mathbf{a} }} \cdot \boldsymbol{\nabla}) {{ \mathbf{b} }} \rangle {}_1" ) ],
                 [ cMathTex( r"=" ),
-                  cMathTex( r"(\boldsymbol{\nabla} \cdot {{ \mathbf{a} }}) {{ \mathbf{b} }} + (\boldsymbol{\nabla} \wedge {{ \mathbf{a} }}) \cdot {{ \mathbf{b} }}"
-                            r"- {{ \mathbf{a} }}(\boldsymbol{\nabla} \cdot {{ \mathbf{b} }})"
-                            r"- {{ \mathbf{a} }} \cdot (\boldsymbol{\nabla} \wedge {{ \mathbf{b} }})" ) ],
-                [ cMathTex( r"\quad+" ),
+                  cMathTex( r"(\boldsymbol{\nabla} \cdot {{ \mathbf{a} }}) {{ \mathbf{b} }}" ),
+                  cMathTex( r" + (\boldsymbol{\nabla} \wedge {{ \mathbf{a} }}) \cdot {{ \mathbf{b} }}" ),
+                  cMathTex( r"- {{ \mathbf{a} }}(\boldsymbol{\nabla} \cdot {{ \mathbf{b} }})" ),
+                  cMathTex( r"- {{ \mathbf{a} }} \cdot (\boldsymbol{\nabla} \wedge {{ \mathbf{b} }})" ) ],
+                [ cMathTex( r"+" ),
                   cMathTex( r"2 ({{ \mathbf{a} }} \cdot \boldsymbol{\nabla}) {{ \mathbf{b} }}" ) ] ]
-        eq3[0][1].next_to( eq2[1][0], DOWN ).shift( 0.0 * LEFT + 0.2 * DOWN )
-        eq3[0][0].next_to( eq3[0][1], LEFT )
-        eq3[0][2].next_to( eq3[0][1], RIGHT )
-        for i in range(3):
-            self.play( Write( eq3[0][i] ) )
-        ref = eq3[0][1]
-        self.wait( 1 )
-        for j in range(3):
-            eq3[j+1][0].next_to( ref, DOWN ).shift( 0.3 * DOWN )
-            eq3[j+1][1].next_to( eq3[j+1][0], RIGHT )
-            for i in range(2):
-                self.play( Write( eq3[j+1][i] ) )
-            self.wait( 1 )
-            ref = eq3[j+1][0]
+
+        aligned( self, eq3, eq2[1][0], 0.0 * LEFT + 0.2 * DOWN, 0.3 * DOWN, 1, 1 )
 
         eq4 = [ [ cMathTex( r"\boldsymbol{\nabla} \cdot ({{ \mathbf{a} }} \wedge {{ \mathbf{b} }})" ),
                   cMathTex( r"=" ),
@@ -99,24 +75,6 @@ class m050_graddot( Scene ):
                   cMathTex( r"+ ({{ \mathbf{a} }} \cdot \boldsymbol{\nabla}) {{ \mathbf{b} }}" ) ] ]
 
         aligned( self, eq4, eq3[3][0], 0.0 * LEFT + 0.2 * DOWN, 0.3 * DOWN, 1, 1 )
-        #ref = eq3[3][0]
-        #eq4[0][1].next_to( ref, DOWN ).shift( 0.0 * LEFT + 0.2 * DOWN )
-        #eq4[0][0].next_to( eq4[0][1], LEFT )
-        #eq4[0][2].next_to( eq4[0][1], RIGHT )
-        #for i in range(3):
-        #    self.play( Write( eq4[0][i] ) )
-        #ref = eq4[0][1]
-        #self.wait( 1 )
-        #alen = len(eq4):
-        #for j in range(alen-1):
-        #    eq4[j+1][0].next_to( ref, DOWN ).shift( 0.3 * DOWN )
-        #    elen = len(eq4[j+1])
-        #    for i in range(elen):
-        #        if i + 1 < elen:
-        #            eq4[j+1][i+1].next_to( eq4[j+1][i], RIGHT )
-        #        self.play( Write( eq4[j+1][i] ) )
-        #    self.wait( 1 )
-        #    ref = eq4[j+1][0]
 
 
         self.wait( 5 )
